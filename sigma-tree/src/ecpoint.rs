de9f53e731acba2ec71628ffe7a7938d558434e1
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-05-27 16:03:26 +0300

    switch to paritytech libsecp256k1;

--- sigma-tree/src/ecpoint.rs
+++ sigma-tree/src/ecpoint.rs
@@ -9,19 +9,19 @@ use std::io;
 pub struct EcPoint(pub PublicKey);
 
 impl EcPoint {
-    pub const PUBLIC_KEY_SIZE: usize = secp256k1::constants::PUBLIC_KEY_SIZE;
+    pub const PUBLIC_KEY_SIZE: usize = secp256k1::util::COMPRESSED_PUBLIC_KEY_SIZE;
 }
 
 impl SigmaSerializable for EcPoint {
     fn sigma_serialize<W: vlq_encode::WriteSigmaVlqExt>(&self, w: &mut W) -> Result<(), io::Error> {
-        w.write_all(&self.0.serialize())?;
+        w.write_all(&self.0.serialize_compressed())?;
         Ok(())
     }
 
     fn sigma_parse<R: vlq_encode::ReadSigmaVlqExt>(r: &mut R) -> Result<Self, SerializationError> {
         let mut bytes = [0; EcPoint::PUBLIC_KEY_SIZE];
         r.read_exact(&mut bytes[..])?;
-        let pk = PublicKey::from_slice(&bytes[..])
+        let pk = PublicKey::parse_compressed(&bytes)
             .map_err(|_| SerializationError::Misc("invalid secp256k1 compressed public key"))?;
         Ok(EcPoint(pk))
     }
@@ -29,13 +29,10 @@ impl SigmaSerializable for EcPoint {
 
 #[cfg(test)]
 mod tests {
-    extern crate rand;
-    extern crate secp256k1;
-
     use super::*;
     use proptest::prelude::*;
     use rand::thread_rng;
-    use secp256k1::Secp256k1;
+    use secp256k1::SecretKey;
     use sigma_ser::test_helpers::*;
 
     impl Arbitrary for EcPoint {
@@ -45,9 +42,9 @@ mod tests {
         fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
             (any::<i32>())
                 .prop_map(|_| {
-                    let secp = Secp256k1::new();
-                    let (_, public_key) = secp.generate_keypair(&mut thread_rng());
-                    EcPoint(public_key)
+                    let sk = SecretKey::random(&mut thread_rng());
+                    let pk = PublicKey::from_secret_key(&sk);
+                    EcPoint(pk)
                 })
                 .boxed()
         }
