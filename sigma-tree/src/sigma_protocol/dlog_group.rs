d6e8836497dfb87a9aa9e754b532448116380eb3
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-08-04 12:01:19 +0300

    add doc comments;

--- sigma-tree/src/sigma_protocol/dlog_group.rs
+++ sigma-tree/src/sigma_protocol/dlog_group.rs
@@ -1,3 +1,20 @@
+//! This is the general interface for the discrete logarithm prime-order group.
+//!
+//! The discrete logarithm problem is as follows: given a generator g of a finite
+//! group G and a random element h in G, find the (unique) integer x such that
+//! `g^x = h`.
+//!
+//! In cryptography, we are interested in groups for which the discrete logarithm problem
+//! (Dlog for short) is assumed to be hard. The most known groups of that kind are some Elliptic curve groups.
+//!
+//! Another issue pertaining elliptic curves is the need to find a suitable mapping that will convert an arbitrary
+//! message (that is some binary string) to an element of the group and vice-versa.
+//!
+//! Only a subset of the messages can be effectively mapped to a group element in such a way that there is a one-to-one
+//! injection that converts the string to a group element and vice-versa.
+//!
+//! On the other hand, any group element can be mapped to some string.
+
 use super::DlogProverInput;
 use k256::arithmetic::{ProjectivePoint, Scalar};
 use k256::{arithmetic::AffinePoint, PublicKey};
@@ -17,30 +34,36 @@ impl EcPoint {
 
 impl Eq for EcPoint {}
 
+/// The generator g of the group is an element of the group such that, when written multiplicatively, every element
+/// of the group is a power of g.
 pub fn generator() -> EcPoint {
     EcPoint(ProjectivePoint::generator())
 }
 
+/// the identity(infinity) element of this Dlog group
 pub const fn identity() -> EcPoint {
     EcPoint(ProjectivePoint::identity())
 }
+
 pub fn is_identity(ge: &EcPoint) -> bool {
     *ge == identity()
 }
 
-pub fn exponentiate(ge: &EcPoint, exponent: &Scalar) -> EcPoint {
-    if !is_identity(ge) {
+/// Raises the base GroupElement to the exponent. The result is another GroupElement.
+pub fn exponentiate(base: &EcPoint, exponent: &Scalar) -> EcPoint {
+    if !is_identity(base) {
         // implement for negative exponent
         // see reference impl https://github.com/ScorexFoundation/sigmastate-interpreter/blob/ec71a6f988f7412bc36199f46e7ad8db643478c7/sigmastate/src/main/scala/sigmastate/basics/BcDlogGroup.scala#L201
         // see https://github.com/ergoplatform/sigma-rust/issues/36
 
         // we treat EC as a multiplicative group, therefore, exponentiate point is multiply.
-        EcPoint(ge.0 * exponent)
+        EcPoint(base.0 * exponent)
     } else {
-        ge.clone()
+        base.clone()
     }
 }
 
+/// Creates a random member of this Dlog group
 pub fn random_element() -> EcPoint {
     let sk = DlogProverInput::random();
     let bytes = sk.w.to_bytes();
@@ -49,6 +72,7 @@ pub fn random_element() -> EcPoint {
     exponentiate(&generator(), &sk.w)
 }
 
+/// Creates a random scalar, a big-endian integer in the range [0, n), where n is group order
 pub fn random_scalar_in_group_range() -> Scalar {
     loop {
         // Generate a new secret key using the operating system's
