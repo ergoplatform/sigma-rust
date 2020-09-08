a8b6aa64e18f49dc36545996aacfda91b7b1c6b5
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-09-08 18:44:34 +0300

    added TxBuiler and tx signing smoke tests in WASM/JS;

--- bindings/ergo-wallet-lib-wasm/tests/test_transaction.js
+++ bindings/ergo-wallet-lib-wasm/tests/test_transaction.js
@@ -1,28 +1,27 @@
 import { expect } from 'chai';
 
 import {
-  Address, Wallet, UnspentBoxes, TxDataInputs, ErgoBoxCandidate, Contract,
-  TxOutputCandidates, ErgoStateContext
+  Address, Wallet, ErgoBoxCandidate, Contract,
+  ErgoBoxes, ErgoBoxCandidates,
+  ErgoStateContext, TxBuilder, BoxValue, UnsignedTransaction
 } from '../pkg/ergo_wallet_lib_wasm';
 
-it('new signed transaction', async () => {
-
+it('TxBuilder test', async () => {
   const recipient = Address.from_testnet_str('3WvsT2Gm4EpsM9Pg18PdY6XyhNNMqXDsvJTbbf6ihLvAmSb7u5RN');
-  const unspent_boxes = UnspentBoxes.from_boxes([]);
-  const tx_data_inputs = TxDataInputs.from_boxes([]);
-  const send_change_to = Address.from_testnet_str('3WvsT2Gm4EpsM9Pg18PdY6XyhNNMqXDsvJTbbf6ihLvAmSb7u5RN');
-
+  const unspent_boxes = ErgoBoxes.from_boxes([]);
   let contract = Contract.pay_to_address(recipient);
   let outbox = new ErgoBoxCandidate(1, 0, contract);
-  let tx_outputs = new TxOutputCandidates(outbox);
-  let dummy_ctx = ErgoStateContext.dummy();
-  let wallet = Wallet.from_mnemonic("", "");
-  expect(() => wallet.new_signed_transaction(dummy_ctx,
-    unspent_boxes,
-    tx_data_inputs,
-    tx_outputs,
-    send_change_to,
-    1,
-    1)).to.throw("Not yet implemented");
+  let tx_outputs = new ErgoBoxCandidates(outbox);
+  let fee = BoxValue.from_u32(1);
+  expect(() => TxBuilder.new(unspent_boxes, tx_outputs, 0, fee)).to.throw("Not yet implemented");
 });
 
+
+it('sign transaction', async () => {
+  const unspent_boxes = ErgoBoxes.from_boxes([]);
+  const tx_data_inputs = ErgoBoxes.from_boxes([]);
+  let dummy_ctx = ErgoStateContext.dummy();
+  let tx = UnsignedTransaction.dummy();
+  let wallet = Wallet.from_mnemonic("", "");
+  expect(() => wallet.sign_transaction(dummy_ctx, tx, unspent_boxes, tx_data_inputs)).to.throw("Not yet implemented");
+});
