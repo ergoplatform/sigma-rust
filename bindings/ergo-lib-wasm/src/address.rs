6579efc621de81c5d9e7fe26d9f2f91995fc05ca
Sebastien Guillemot <SebastienGllmt@gmail.com>
2020-11-06 22:00:31 +0900

    First draft of extended wasm addresses (#133)
    * WIP address functions

--- bindings/ergo-lib-wasm/src/address.rs
+++ bindings/ergo-lib-wasm/src/address.rs
@@ -2,6 +2,80 @@
 
 use ergo_lib::chain;
 use wasm_bindgen::prelude::*;
+use ergo_lib::{
+    serialization::{SigmaSerializable},
+    sigma_protocol::{
+        dlog_group::EcPoint,
+        sigma_boolean::{ProveDlog},
+    },
+};
+
+/// Network type
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
+pub enum NetworkPrefix {
+    /// Mainnet
+    Mainnet = 0,
+    /// Testnet
+    Testnet = 16,
+}
+
+impl From<NetworkPrefix> for chain::address::NetworkPrefix {
+    fn from(v: NetworkPrefix) -> Self {
+        use chain::address::NetworkPrefix::*;
+        match v {
+            NetworkPrefix::Mainnet => Mainnet,
+            NetworkPrefix::Testnet => Testnet,
+        }
+    }
+}
+
+impl From<chain::address::NetworkPrefix> for NetworkPrefix {
+    fn from(v: chain::address::NetworkPrefix) -> Self {
+        use NetworkPrefix::*;
+        match v {
+            chain::address::NetworkPrefix::Mainnet => Mainnet,
+            chain::address::NetworkPrefix::Testnet => Testnet,
+        }
+    }
+}
+
+
+/// Address types
+#[wasm_bindgen]
+#[repr(u8)]
+pub enum AddressTypePrefix {
+    /// 0x01 - Pay-to-PublicKey(P2PK) address
+    P2PK = 1,
+    /// 0x02 - Pay-to-Script-Hash(P2SH)
+    Pay2SH = 2,
+    /// 0x03 - Pay-to-Script(P2S)
+    Pay2S = 3,
+}
+
+impl From<AddressTypePrefix> for chain::address::AddressTypePrefix {
+    fn from(v: AddressTypePrefix) -> Self {
+        use chain::address::AddressTypePrefix::*;
+        match v {
+            AddressTypePrefix::P2PK => P2PK,
+            AddressTypePrefix::Pay2SH => Pay2SH,
+            AddressTypePrefix::Pay2S => Pay2S,
+        }
+    }
+}
+
+impl From<chain::address::AddressTypePrefix> for AddressTypePrefix {
+    fn from(v: chain::address::AddressTypePrefix) -> Self {
+        use AddressTypePrefix::*;
+        match v {
+            chain::address::AddressTypePrefix::P2PK => P2PK,
+            chain::address::AddressTypePrefix::Pay2SH => Pay2SH,
+            chain::address::AddressTypePrefix::Pay2S => Pay2S,
+        }
+    }
+}
+
 
 /**
  * An address is a short string corresponding to some script used to protect a box. Unlike (string-encoded) binary
@@ -56,6 +130,39 @@ impl Address {
     pub fn from_testnet_str(s: &str) -> Result<Address, JsValue> {
         chain::address::AddressEncoder::new(chain::address::NetworkPrefix::Testnet)
             .parse_address_from_str(s)
+            .map(Address)
+            .map_err(|e| JsValue::from_str(&format!("{}", e)))
+    }
+
+    /// Decode (base58) mainnet address from string
+    pub fn from_mainnet_str(s: &str) -> Result<Address, JsValue> {
+        chain::address::AddressEncoder::new(chain::address::NetworkPrefix::Mainnet)
+            .parse_address_from_str(s)
+            .map(Address)
+            .map_err(|e| JsValue::from_str(&format!("{}", e)))
+    }
+
+    /// Decode (base58) address from string
+    pub fn from_base58(s: &str) -> Result<Address, JsValue> {
+        chain::address::AddressEncoder::unchecked_parse_address_from_str(s)
+            .map(Address)
+            .map_err(|e| JsValue::from_str(&format!("{}", e)))
+    }
+
+    /// Encode (base58) address
+    pub fn to_base58(&self, network_prefix: NetworkPrefix) -> String {
+        chain::address::AddressEncoder::encode_address(network_prefix.into(), &self.0)
+    }
+
+    /// Get the type of the address
+    pub fn address_type_prefix(&self) -> AddressTypePrefix {
+        self.0.address_type_prefix().into()
+    }
+
+    /// Create an address from a public key
+    pub fn from_public_key(bytes: &[u8]) -> Result<Address, JsValue> {
+        EcPoint::sigma_parse_bytes(bytes.to_vec())
+            .map(|point| chain::address::Address::P2PK(ProveDlog::new(point)))
             .map(Address)
             .map_err(|e| JsValue::from_str(&format!("{}", e)))
     }
