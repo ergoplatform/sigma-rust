cca6fd640a01929fd4b985779aaf411de85638a3
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-12-08 16:44:19 +0200

    draft MethodCall serialization;

--- ergo-lib/src/serialization/method_call.rs
+++ ergo-lib/src/serialization/method_call.rs
@@ -1,6 +1,10 @@
 use std::io::Error;
 
+use crate::ast::expr::Expr;
 use crate::ast::method_call::MethodCall;
+use crate::types::smethod::MethodId;
+use crate::types::smethod::SMethod;
+use crate::types::stype_companion::TypeId;
 
 use super::sigma_byte_reader::SigmaByteRead;
 use super::sigma_byte_writer::SigmaByteWrite;
@@ -9,21 +13,28 @@ use super::SigmaSerializable;
 
 impl SigmaSerializable for MethodCall {
     fn sigma_serialize<W: SigmaByteWrite>(&self, w: &mut W) -> Result<(), Error> {
-        w.put_u8(self.method.obj_type.type_id().0)?;
-        w.put_u8(self.method.method_id().0)?;
+        self.method.obj_type.type_id().sigma_serialize(w)?;
+        self.method.method_id().sigma_serialize(w)?;
         self.obj.sigma_serialize(w)?;
-        // TODO: return error on empty args
-        if !self.args.is_empty() {
-            self.args.iter().try_for_each(|a| a.sigma_serialize(w))?;
-        }
+        w.put_u32(self.args.len() as u32)?;
+        self.args.iter().try_for_each(|a| a.sigma_serialize(w))?;
         Ok(())
     }
 
     fn sigma_parse<R: SigmaByteRead>(r: &mut R) -> Result<Self, SerializationError> {
-        todo!()
-        // let type_id = r.get_u8()?;
-        // let method_id = r.get_u8()?;
-        // let obj = Expr::sigma_parse(r)?;
+        let type_id = TypeId::sigma_parse(r)?;
+        let method_id = MethodId::sigma_parse(r)?;
+        let obj = Expr::sigma_parse(r)?;
+        let args_count = r.get_u32()?;
+        let mut args = Vec::with_capacity(args_count as usize);
+        for _ in 0..args_count {
+            args.push(Expr::sigma_parse(r)?);
+        }
+        Ok(MethodCall {
+            obj: Box::new(obj),
+            method: SMethod::from_ids(type_id, method_id),
+            args,
+        })
     }
 }
 
