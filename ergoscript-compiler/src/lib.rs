5d916f71bbf70cbeaad60933023c9b8714ca1b3d
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-02-16 13:39:22 +0200

    add code convention lints;

--- ergoscript-compiler/src/lib.rs
+++ ergoscript-compiler/src/lib.rs
@@ -1,3 +1,18 @@
+//! ErgoScript compiler pipeline
+
+// Coding conventions
+#![forbid(unsafe_code)]
+#![deny(non_upper_case_globals)]
+#![deny(non_camel_case_types)]
+#![deny(non_snake_case)]
+#![deny(unused_mut)]
+// #![deny(dead_code)]
+#![deny(unused_imports)]
+#![deny(missing_docs)]
+// Clippy exclusions
+#![allow(clippy::unit_arg)]
+#![deny(broken_intra_doc_links)]
+
 mod ast;
 mod hir;
 mod lexer;
