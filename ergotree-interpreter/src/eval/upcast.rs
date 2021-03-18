b64f267f4f4f469cb1f3ece38c8d4a0d893f22f1
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-03-18 14:53:40 +0200

    Upcast eval;

--- ergotree-interpreter/src/eval/upcast.rs
+++ ergotree-interpreter/src/eval/upcast.rs
@@ -1,11 +1,26 @@
 use ergotree_ir::mir::upcast::Upcast;
 use ergotree_ir::mir::value::Value;
 use ergotree_ir::types::stype::SType;
+use num_bigint::ToBigInt;
 
 use crate::eval::env::Env;
 use crate::eval::EvalContext;
 use crate::eval::EvalError;
 use crate::eval::Evaluable;
+
+fn upcast_to_bigint(in_v: Value) -> Result<Value, EvalError> {
+    match in_v {
+        Value::Byte(v) => Ok(v.to_bigint().into()),
+        Value::Short(v) => Ok(v.to_bigint().into()),
+        Value::Int(v) => Ok(v.to_bigint().into()),
+        Value::Long(v) => Ok(v.to_bigint().into()),
+        Value::BigInt(_) => Ok(in_v),
+        _ => Err(EvalError::UnexpectedValue(format!(
+            "Upcast: cannot upcast {0:?} to Long",
+            in_v
+        ))),
+    }
+}
 
 fn upcast_to_long(in_v: Value) -> Result<Value, EvalError> {
     match in_v {
@@ -57,7 +72,7 @@ impl Evaluable for Upcast {
     fn eval(&self, env: &Env, ctx: &mut EvalContext) -> Result<Value, EvalError> {
         let input_v = self.input.eval(env, ctx)?;
         match self.tpe {
-            SType::SBigInt => todo!("upcasting to BigInt is not yet supported"),
+            SType::SBigInt => upcast_to_bigint(input_v),
             SType::SLong => upcast_to_long(input_v),
             SType::SInt => upcast_to_int(input_v),
             SType::SShort => upcast_to_short(input_v),
