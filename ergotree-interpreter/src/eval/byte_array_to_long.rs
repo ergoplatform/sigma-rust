c3b6ec19794b1a4f53642d80a386aaa08193627e
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-04-26 11:58:46 +0300

    Module mixup

--- ergotree-interpreter/src/eval/byte_array_to_long.rs
+++ ergotree-interpreter/src/eval/byte_array_to_long.rs
@@ -1,4 +1,4 @@
-use ergotree_ir::mir::byte_array_to_bigint::ByteArrayToBigInt;
+use ergotree_ir::mir::byte_array_to_long::ByteArrayToLong;
 use ergotree_ir::mir::value::Value;
 
 use crate::eval::env::Env;
@@ -7,7 +7,7 @@ use crate::eval::EvalError;
 use crate::eval::Evaluable;
 use ergotree_ir::mir::constant::TryExtractInto;
 
-impl Evaluable for ByteArrayToBigInt {
+impl Evaluable for ByteArrayToLong {
     fn eval(&self, env: &Env, ctx: &mut EvalContext) -> Result<Value, EvalError> {
         let _input_v = self.input.eval(env, ctx)?.try_extract_into::<Vec<i8>>()?;
         todo!();
