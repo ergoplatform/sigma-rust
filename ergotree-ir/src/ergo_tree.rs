3599ad7865f2383a8c51373184755259e54bcde1
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-06-01 13:35:24 +0300

    implement `ErgoTree::version()`;

--- ergotree-ir/src/ergo_tree.rs
+++ ergotree-ir/src/ergo_tree.rs
@@ -97,10 +97,18 @@ pub struct ErgoTreeHeader(u8);
 pub struct ErgoTreeVersion(u8);
 
 impl ErgoTreeVersion {
+    /// Header mask to extract version bits.
+    pub const VERSION_MASK: u8 = 0x07;
     /// Version 0
     pub const V0: Self = ErgoTreeVersion(0);
     /// Version 1 (size flag is mandatory)
     pub const V1: Self = ErgoTreeVersion(1);
+
+    /// Returns a value of the version bits from the given header byte.
+    pub fn parse_version(header: &ErgoTreeHeader) -> ErgoTreeVersion {
+        let header_byte: u8 = header.clone().into();
+        ErgoTreeVersion(header_byte & ErgoTreeVersion::VERSION_MASK)
+    }
 }
 
 impl ErgoTreeHeader {
@@ -141,7 +149,7 @@ impl ErgoTreeHeader {
 
     /// Returns ErgoTree version
     pub fn version(&self) -> ErgoTreeVersion {
-        todo!()
+        ErgoTreeVersion::parse_version(self)
     }
 }
 
@@ -640,5 +648,21 @@ mod tests {
             false.into()
         );
         assert_eq!(ergo_tree.get_constant(0).unwrap().unwrap(), true.into());
+    }
+
+    #[test]
+    fn dex_t2tpool_parse() {
+        let base16_str = "19a3030f0400040204020404040404060406058080a0f6f4acdbe01b058080a0f6f4acdbe01b050004d00f0400040005000500d81ad601b2a5730000d602e4c6a70405d603db63087201d604db6308a7d605b27203730100d606b27204730200d607b27203730300d608b27204730400d609b27203730500d60ab27204730600d60b9973078c720602d60c999973088c720502720bd60d8c720802d60e998c720702720dd60f91720e7309d6108c720a02d6117e721006d6127e720e06d613998c7209027210d6147e720d06d615730ad6167e721306d6177e720c06d6187e720b06d6199c72127218d61a9c72167218d1edededededed93c27201c2a793e4c672010405720292c17201c1a793b27203730b00b27204730c00938c7205018c720601ed938c7207018c720801938c7209018c720a019593720c730d95720f929c9c721172127e7202069c7ef07213069a9c72147e7215067e9c720e720206929c9c721472167e7202069c7ef0720e069a9c72117e7215067e9c721372020695ed720f917213730e907217a19d721972149d721a7211ed9272199c7217721492721a9c72177211";
+        let tree_bytes = base16::decode(base16_str.as_bytes()).unwrap();
+        let mut tree = ErgoTree::sigma_parse_bytes(&tree_bytes).unwrap();
+        dbg!(&tree);
+        assert!(tree.header.has_size());
+        assert!(tree.header.is_constant_segregation());
+        assert_eq!(tree.header.version(), ErgoTreeVersion::V1);
+        tree.set_constant(7, 1i64.into()).unwrap();
+        assert_eq!(tree.get_constant(7).unwrap().unwrap(), 1i64.into());
+        tree.set_constant(8, 2i64.into()).unwrap();
+        assert_eq!(tree.get_constant(8).unwrap().unwrap(), 2i64.into());
+        assert!(tree.sigma_serialize_bytes().len() > 1);
     }
 }
