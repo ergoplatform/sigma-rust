e2d7bf8b3621405712f7a3c263fd12b53b5ecbbb
ApexTheory <blind.apex@gmail.com>
2021-06-12 09:27:49 -0400

    Code improvements and change to Vec<i8> to match NativeColl

--- ergotree-interpreter/src/eval/xor.rs
+++ ergotree-interpreter/src/eval/xor.rs
@@ -8,8 +8,8 @@ use crate::eval::EvalContext;
 use crate::eval::EvalError;
 use crate::eval::Evaluable;
 
-fn _helper_xor(mut x : Vec<u8>, y : Vec<u8>) -> Vec<u8> {
-    
+fn _helper_xor(mut x : Vec<i8>, y : Vec<i8>) -> Vec<i8> {
+
     x.iter_mut().zip(y.iter()).for_each(|(x1, x2)| *x1 ^= *x2);
     x
 }
@@ -20,13 +20,11 @@ impl Evaluable for Xor {
         let right_v = self.right.eval(env, ctx)?;
 
         match (left_v.clone(), right_v.clone()) {
-            (Value::Coll(CollKind::NativeColl(NativeColl::CollByte(mut l_byte))),
+            (Value::Coll(CollKind::NativeColl(NativeColl::CollByte(l_byte))),
             Value::Coll(CollKind::NativeColl(NativeColl::CollByte(r_byte)))) => {
-                    l_byte.iter_mut()
-                        .zip(r_byte.iter())
-                        .for_each(|(x1, x2)| *x1 ^= *x2);
-                        
-                Ok(l_byte.into())
+                    
+                let xor = _helper_xor(l_byte.clone(), r_byte.clone());                        
+                Ok(xor.into())
             }
             _ => Err(EvalError::UnexpectedValue(format!(
                 "expected Xor input to be byte array, got: {0:?}",
@@ -50,9 +48,9 @@ mod tests {
     #[test]
     fn eval_1_xor_0() {
 
-        let left = vec![1 as u8];
-        let right = vec![0 as u8];
-        let expected_xor = vec![1 as u8];
+        let left = vec![1 as i8];
+        let right = vec![0 as i8];
+        let expected_xor = vec![1 as i8];
         
         let expr: Expr = Xor {
             left: Box::new(Expr::Const(left.into())),
@@ -61,15 +59,15 @@ mod tests {
         .into();
 
         let ctx = Rc::new(force_any_val::<Context>());
-        assert_eq!(eval_out::<Vec<u8>>(&expr, ctx), expected_xor);
+        assert_eq!(eval_out::<Vec<i8>>(&expr, ctx), expected_xor);
     }
 
     #[test]
     fn eval_0_xor_1() {
 
-        let left = vec![0 as u8];
-        let right = vec![1 as u8];
-        let expected_xor = vec![1 as u8];
+        let left = vec![0 as i8];
+        let right = vec![1 as i8];
+        let expected_xor = vec![1 as i8];
         
         let expr: Expr = Xor {
             left: Box::new(Expr::Const(left.into())),
@@ -78,15 +76,15 @@ mod tests {
         .into();
 
         let ctx = Rc::new(force_any_val::<Context>());
-        assert_eq!(eval_out::<Vec<u8>>(&expr, ctx), expected_xor);
+        assert_eq!(eval_out::<Vec<i8>>(&expr, ctx), expected_xor);
     }
 
     #[test]
     fn eval_1_xor_1() {
 
-        let left = vec![1 as u8];
-        let right = vec![1 as u8];
-        let expected_xor = vec![0 as u8];
+        let left = vec![1 as i8];
+        let right = vec![1 as i8];
+        let expected_xor = vec![0 as i8];
         
         let expr: Expr = Xor {
             left: Box::new(Expr::Const(left.into())),
@@ -95,15 +93,15 @@ mod tests {
         .into();
 
         let ctx = Rc::new(force_any_val::<Context>());
-        assert_eq!(eval_out::<Vec<u8>>(&expr, ctx), expected_xor);
+        assert_eq!(eval_out::<Vec<i8>>(&expr, ctx), expected_xor);
     }
 
     #[test]
     fn eval_0_xor_0() {
 
-        let left = vec![0 as u8];
-        let right = vec![0 as u8];
-        let expected_xor = vec![0 as u8];
+        let left = vec![0 as i8];
+        let right = vec![0 as i8];
+        let expected_xor = vec![0 as i8];
         
         let expr: Expr = Xor {
             left: Box::new(Expr::Const(left.into())),
@@ -112,15 +110,15 @@ mod tests {
         .into();
 
         let ctx = Rc::new(force_any_val::<Context>());
-        assert_eq!(eval_out::<Vec<u8>>(&expr, ctx), expected_xor);
+        assert_eq!(eval_out::<Vec<i8>>(&expr, ctx), expected_xor);
     }
 
     #[test]
     fn eval_1100_xor_0101() {
 
-        let left = vec![1 as u8, 1 as u8, 0 as u8, 0 as u8];
-        let right = vec![0 as u8, 1 as u8, 0 as u8, 1 as u8];
-        let expected_xor = vec![1 as u8, 0 as u8, 0 as u8, 1 as u8];
+        let left = vec![1 as i8, 1 as i8, 0 as i8, 0 as i8];
+        let right = vec![0 as i8, 1 as i8, 0 as i8, 1 as i8];
+        let expected_xor = vec![1 as i8, 0 as i8, 0 as i8, 1 as i8];
         
         let expr: Expr = Xor {
             left: Box::new(Expr::Const(left.into())),
@@ -129,13 +127,13 @@ mod tests {
         .into();
 
         let ctx = Rc::new(force_any_val::<Context>());
-        assert_eq!(eval_out::<Vec<u8>>(&expr, ctx), expected_xor);
+        assert_eq!(eval_out::<Vec<i8>>(&expr, ctx), expected_xor);
     }
 
     proptest! {
 
         #[test]
-        fn eval_any(left_bytes in any::<Vec<u8>>(), right_bytes in any::<Vec<u8>>()) {
+        fn eval_any(left_bytes in any::<Vec<i8>>(), right_bytes in any::<Vec<i8>>()) {
 
             let expected_xor = _helper_xor(left_bytes.clone(), right_bytes.clone());
             
@@ -146,7 +144,7 @@ mod tests {
             .into();
 
             let ctx = Rc::new(force_any_val::<Context>());
-            assert_eq!(eval_out::<Vec<u8>>(&expr, ctx), expected_xor);
+            assert_eq!(eval_out::<Vec<i8>>(&expr, ctx), expected_xor);
         }
     }
 }
