aacdca38a5658c8af9527ad1003db09bb92fbcda
Andreas <none@none.com>
2021-06-13 12:39:35 +0200

    Fix to branch issue_nr_301 suggested by code review
     - Renamed output to col_2
     - Better Tests for Eval trait (includes empty vectors)
     - Cleaned commented code

--- ergotree-interpreter/src/eval/coll_append.rs
+++ ergotree-interpreter/src/eval/coll_append.rs
@@ -14,27 +14,21 @@ fn _helper_concat<T>(mut e1 : Vec<T>, e2 : Vec<T>) -> Vec<T> {
     e1
 }
 
+fn _helper_extract_vecval(inp : Value) -> Result<Vec<Value>, EvalError> {
+    match inp {
+        Value::Coll(coll) => Ok(coll.as_vec()),
+        _ => Err(EvalError::UnexpectedValue(format!(
+            "Append: expected Value to be Value::Coll, got: {0:?}",
+            inp
+        ))),
+    }
+}
+
 impl Evaluable for Append {
     fn eval(&self, env: &Env, ctx: &mut EvalContext) -> Result<Value, EvalError> {
-        // let vec = vec![];
-        // Ok(Value::from(vec![1_u8]))
-        let input_val : Value = self.input.eval(env, ctx)?;
-        let output_val : Value = self.output.eval(env, ctx)?;
-        let input_vecval : Vec<Value> = match input_val {
-            Value::Coll(coll) => Ok(coll.as_vec()),
-            _ => Err(EvalError::UnexpectedValue(format!(
-                "Append: expected input to be Value::Coll, got: {0:?}",
-                input_val
-            ))),
-        }?;
-        let output_vecval : Vec<Value> = match output_val {
-            Value::Coll(coll) => Ok(coll.as_vec()),
-            _ => Err(EvalError::UnexpectedValue(format!(
-                "Append: expected output to be Value::Coll, got: {0:?}",
-                output_val
-            ))),
-        }?;
-        let concat_vecval : Vec<Value> = _helper_concat(input_vecval, output_vecval);
+        let input_vecval : Vec<Value> = _helper_extract_vecval(self.input.eval(env, ctx)?)?;
+        let col_2_vecval : Vec<Value> = _helper_extract_vecval(self.col_2.eval(env, ctx)?)?;
+        let concat_vecval : Vec<Value> = _helper_concat(input_vecval, col_2_vecval);
         Ok(Value::Coll(CollKind::from_vec(self.tpe(), concat_vecval)?))
     }
 }
@@ -42,79 +36,38 @@ impl Evaluable for Append {
 #[allow(clippy::unwrap_used)]
 #[cfg(test)]
 mod tests {
-    // use ergotree_ir::ir_ergo_box::IrBoxId;
     use ergotree_ir::mir::expr::Expr;
-    // use ergotree_ir::mir::global_vars::GlobalVars;
-    // use sigma_test_util::force_any_val;
-
     use super::*;
-    // use crate::eval::context::Context;
-    // use crate::eval::tests::eval_out;
     use crate::eval::tests::eval_out_wo_ctx;
-    // use std::rc::Rc;
+
+    // Comment here for an elegant solution - generic single_test
+    // Fails due to Expr::from needing complicated type bounds
+    // Fails both with T and Vec<T>
+    // fn test_single_append<T : PartialEq + Debug + LiftIntoSType> (inp : T, inp2 : T, expected_output : T) {
+    //     let in_expr = Expr::from(inp);
+    //     let col2_expr = Expr::from(inp2);
+    //     let append_expr = Expr::from(Append::new(in_expr, col2_expr).unwrap());
+    //     let append_eval : Vec<T> = eval_out_wo_ctx(append_expr);
+    //     assert_eq!(append_eval, expected_output);
+    // }
 
     #[test]
-    fn basic_eval() {
-
-        let in_expr : Expr = Expr::from(vec![1,2,3,4]);
-        let out_expr : Expr = Expr::from(vec![5,6,7,8]);
-        let construct_append : Expr = Append::new(in_expr, out_expr).unwrap().into();
-        let append_eval = eval_out_wo_ctx::<Vec<i32>>(&construct_append);
-        // let append_expr : Value = .eval(ctx, env).unwrap();
-        let concat_expr = Expr::from(vec![1,2,3,4,5,6,7,8]);
-        let concat_eval = eval_out_wo_ctx::<Vec<i32>>(&concat_expr);
-
-        assert_eq!(append_eval, concat_eval);
-        // let expr: Expr = Append::new(GlobalVars::Inputs.into(), GlobalVars::Outputs.into())
-        //     .unwrap()
-        //     .into();
-        // let ctx = Rc::new(force_any_val::<Context>());
-        // assert_eq!(
-        //     eval_out::<IrBoxId>(&expr, ctx.clone()),
-        //     ctx.outputs.get(0).unwrap().clone()
-        // );
+    fn test_append_basic_numbers() {
+        for (test_inp, test_col2, expected_output) in [
+                (vec![1,2,3,4], vec![5,6,7,8], vec![1,2,3,4,5,6,7,8]),
+                (vec![], vec![], vec![]),
+                (vec![], vec![1], vec![1]),
+                (vec![], vec![2,2], vec![2,2]),
+                (vec![1], vec![], vec![1]),
+                (vec![2,2], vec![], vec![2,2]),
+                (vec![1], vec![2,2], vec![1,2,2]),
+        ].iter() {
+            let in_expr = Expr::from(test_inp.clone());
+            let col2_expr = Expr::from(test_col2.clone());
+            let append_expr = Expr::from(Append::new(in_expr, col2_expr).unwrap());
+            let append_eval : Vec<i32> = eval_out_wo_ctx(&append_expr);
+            assert_eq!(append_eval, *expected_output);
+        }
     }
-
    
 }
-
-
-        // #[test]
-        // fn eval_box_value(ctx in any::<Context>()) {
-        //     let data_inputs: Expr = PropertyCall::new(Expr::Context, scontext::DATA_INPUTS_PROPERTY.clone()).unwrap()
-        //     .into();
-        //     let val_use: Expr = ValUse {
-        //         val_id: 1.into(),
-        //         tpe: SType::SBox,
-        //     }
-        //     .into();
-        //     let mapper_body: Expr = BinOp {
-        //         kind: ArithOp::Plus.into(),
-        //         left: Box::new(Expr::Const(1i64.into())),
-        //         right: Box::new(Expr::ExtractAmount(
-        //                 ExtractAmount::try_build(val_use)
-        //             .unwrap(),
-        //         )),
-        //     }
-        //     .into();
-        //     let expr: Expr = Map::new(
-        //         data_inputs,
-        //         FuncValue::new(
-        //             vec![FuncArg {
-        //                 idx: 1.into(),
-        //                 tpe: SType::SBox,
-        //             }],
-        //             mapper_body,
-        //         )
-        //         .into(),
-        //     )
-        //     .unwrap()
-        //     .into();
-        //     let ctx = Rc::new(ctx);
-        //     assert_eq!(
-        //         eval_out::<Vec<i64>>(&expr, ctx.clone()),
-        //         ctx.data_inputs
-        //             .iter()
-        //             .map(| b| b.get_box(&ctx.box_arena).unwrap().value() + 1).collect::<Vec<i64>>()
-        //     );
-        // }
--- ergotree-ir/src/mir/coll_append.rs
+++ ergotree-ir/src/mir/coll_append.rs
@@ -11,33 +11,33 @@ use super::expr::Expr;
 use super::expr::InvalidArgumentError;
 use crate::has_opcode::HasStaticOpCode;
 
-/// Takes two collections as input and concatenates both into the Append Colelction
+/// Takes two collections as input and produces the concatenated collection (input + col2)
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct Append {
     /// Collection - First Parameter; first half of the combined collection
     pub input: Box<Expr>,
     /// Collection - Second Parameter; later half of the combined collection
-    pub output: Box<Expr>,
+    pub col_2: Box<Expr>,
 }
 
 impl Append {
     /// Create new object, returns an error if any of the requirements failed
-    pub fn new(input: Expr, output: Expr) -> Result<Self, InvalidArgumentError> {
+    pub fn new(input: Expr, col_2: Expr) -> Result<Self, InvalidArgumentError> {
         let first_elem_type : SType = input.post_eval_tpe();
-        let second_elem_type : SType = output.post_eval_tpe();
+        let second_elem_type : SType = col_2.post_eval_tpe();
         match (first_elem_type, second_elem_type) {
             (SType::SColl(x), SType::SColl(y)) => {
                 if x == y {
-                    Ok(Append{input: input.into(), output: output.into()})
+                    Ok(Append{input: input.into(), col_2: col_2.into()})
                 } else {
                     Err(InvalidArgumentError(format!(
-                        "Expected Append input and output collection to have the same types; got input={0:?} output={1:?}",
+                        "Expected Append input and col_2 collection to have the same types; got input={0:?} col_2={1:?}",
                         x, y)))
                 }
             }
             (SType::SColl(_), _) => {
                 Err(InvalidArgumentError(format!(
-                    "Expected Append output param to be a collection; got output={:?}", output.tpe())))
+                    "Expected Append col_2 param to be a collection; got col_2={:?}", col_2.tpe())))
             }
             (_, SType::SColl(_)) => {
                 Err(InvalidArgumentError(format!(
@@ -45,14 +45,14 @@ impl Append {
             },
             (_, _) => {
                 Err(InvalidArgumentError(format!(
-                    "Expected Append input and output param to be a collection; got input={:?} output={:?}", input.tpe(), output.tpe())))   
+                    "Expected Append input and col_2 param to be a collection; got input={:?} col_2={:?}", input.tpe(), col_2.tpe())))   
             }
         }
     }
 
     /// Type
     pub fn tpe(&self) -> SType {
-        // Type is supposed to be the same on input and output
+        // Type is supposed to be the same on input and col_2
         // Append::new checks types but later modifications are unchecked
         // return type of first element
         self.input.tpe()
@@ -67,15 +67,15 @@ impl HasStaticOpCode for Append {
 impl SigmaSerializable for Append {
     fn sigma_serialize<W: SigmaByteWrite>(&self, w: &mut W) -> Result<(), std::io::Error> {
         self.input.sigma_serialize(w)?;
-        self.output.sigma_serialize(w)?;
+        self.col_2.sigma_serialize(w)?;
         Ok(())
     }
 
     fn sigma_parse<R: SigmaByteRead>(r: &mut R) -> Result<Self, SerializationError> {
         let input = Expr::sigma_parse(r)?.into();
-        let output = Expr::sigma_parse(r)?.into();
+        let col_2 = Expr::sigma_parse(r)?.into();
         // InvalidArgumentError can create SerializationError; no map_err needed
-        Ok(Append::new(input, output)?)
+        Ok(Append::new(input, col_2)?)
     }
 }
 
@@ -103,9 +103,9 @@ mod tests {
                     depth: 1,
                 }),
             )                
-                .prop_map(|(input, output)| Self {
+                .prop_map(|(input, col_2)| Self {
                     input: input.into(),
-                    output: output.into(),
+                    col_2: col_2.into(),
                 })
                 .boxed()
         }
