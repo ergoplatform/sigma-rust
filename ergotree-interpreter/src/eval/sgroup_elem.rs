3d966fb40be0e2762f43a5a52583007cff9b7355
ApexTheory <blind.apex@gmail.com>
2021-07-06 13:58:21 -0400

    331 - GroupElement - Negate

---
+++ ergotree-interpreter/src/eval/sgroup_elem.rs
@@ -0,0 +1,44 @@
+use crate::eval::EvalError;
+
+use ergotree_ir::mir::value::Value;
+use ergotree_ir::sigma_protocol::dlog_group::EcPoint;
+
+use super::EvalFn;
+
+pub(crate) static NEGATE_EVAL_FN: EvalFn = |_env, _ctx, obj, _args| {
+    println!("IN NEGATE: {:?}", obj);
+    let negated: EcPoint = match obj {
+        Value::GroupElement(ec_point) => Ok(-(*ec_point)),
+        _ => Err(EvalError::UnexpectedValue(format!(
+            "expected obj to be Value::GroupElement, got: {0:?}",
+            obj
+        ))),
+    }?;
+    Ok(Value::GroupElement(Box::new(negated)))
+};
+
+#[allow(clippy::unwrap_used)]
+#[cfg(test)]
+#[cfg(feature = "arbitrary")]
+mod tests {
+    use ergotree_ir::mir::expr::Expr;
+    use ergotree_ir::mir::method_call::MethodCall;
+    use ergotree_ir::types::sgroup_elem;
+
+    use crate::eval::tests::eval_out_wo_ctx;
+    use ergotree_ir::sigma_protocol::dlog_group::EcPoint;
+    use sigma_test_util::force_any_val;
+
+    #[test]
+    fn eval_negate() {
+        let input = force_any_val::<EcPoint>();
+        let expr: Expr = MethodCall::new(
+            input.clone().into(),
+            sgroup_elem::NEGATE_METHOD.clone(),
+            vec![],
+        )
+        .unwrap()
+        .into();
+        assert_eq!(-input, eval_out_wo_ctx::<EcPoint>(&expr))
+    }
+}
---
+++ ergotree-ir/src/types/sgroup_elem.rs
@@ -0,0 +1,44 @@
+use crate::serialization::types::TypeCode;
+
+use super::sfunc::SFunc;
+use super::smethod::MethodId;
+use super::smethod::SMethod;
+use super::smethod::SMethodDesc;
+use super::stype::SType;
+use super::stype_companion::STypeCompanion;
+use super::stype_companion::STypeCompanionHead;
+use lazy_static::lazy_static;
+
+/// SBox type id
+pub const TYPE_ID: TypeCode = TypeCode::SGROUP_ELEMENT;
+/// Box.value property
+pub const NEGATE_METHOD_ID: MethodId = MethodId(5);
+
+static S_GROUP_ELEM_TYPE_COMPANION_HEAD: STypeCompanionHead = STypeCompanionHead {
+    type_id: TYPE_ID,
+    type_name: "GroupElement",
+};
+
+lazy_static! {
+    /// GroupElement object type companion
+    pub static ref S_GROUP_ELEM_TYPE_COMPANION: STypeCompanion = STypeCompanion::new(
+        &S_GROUP_ELEM_TYPE_COMPANION_HEAD,
+        vec![
+            &NEGATE_METHOD_DESC
+        ]
+    );
+}
+
+lazy_static! {
+    static ref NEGATE_METHOD_DESC: SMethodDesc = SMethodDesc {
+        method_id: NEGATE_METHOD_ID,
+        name: "negate",
+        tpe: SFunc {
+            t_dom: vec![SType::SGroupElement],
+            t_range: Box::new(SType::SGroupElement),
+            tpe_params: vec![],
+        },
+    };
+    /// GroupElement.negate
+    pub static ref NEGATE_METHOD: SMethod = SMethod::new(&S_GROUP_ELEM_TYPE_COMPANION, NEGATE_METHOD_DESC.clone(),);
+}
