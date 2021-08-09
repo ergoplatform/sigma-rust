00684885589b7d6d06d02426fb9724aae039a69c
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-08-09 16:03:35 +0300

    add and update comments;

--- ergotree-ir/src/serialization/types.rs
+++ ergotree-ir/src/serialization/types.rs
@@ -13,7 +13,7 @@ use num_traits::FromPrimitive;
 use std::convert::TryInto;
 
 #[allow(non_camel_case_types)]
-#[allow(clippy::upper_case_acronyms)]
+#[allow(clippy::upper_case_acronyms)] // to differentiate from similarly named SType enum variants
 #[derive(Copy, Clone, Debug, PartialEq, Eq, FromPrimitive)]
 #[repr(u8)]
 pub enum TypeCode {
@@ -327,17 +327,15 @@ impl SType {
     }
 }
 
-/**
- * Each SType is serialized to array of bytes by:
- * - emitting typeCode of each node (see special case for collections below)
- * - then recursively serializing subtrees from left to right on each level
- * - for each collection of primitive type there is special type code to emit single byte instead of two bytes
- * Types code intervals
- * - (1 .. MaxPrimTypeCode)  // primitive types
- * - (CollectionTypeCode .. CollectionTypeCode + MaxPrimTypeCode) // collections of primitive types
- * - (MaxCollectionTypeCode ..)  // Other types
- * Collection of non-primitive type is serialized as (CollectionTypeCode, serialize(elementType))
- */
+/// Each SType is serialized to array of bytes by:
+/// - emitting typeCode of each node (see special case for collections below)
+/// - then recursively serializing subtrees from left to right on each level
+/// - for each collection of primitive type there is special type code to emit single byte instead of two bytes
+/// Types code intervals
+/// - (1 .. MaxPrimTypeCode)  // primitive types
+/// - (CollectionTypeCode .. CollectionTypeCode + MaxPrimTypeCode) // collections of primitive types
+/// - (MaxCollectionTypeCode ..)  // Other types
+/// Collection of non-primitive type is serialized as (CollectionTypeCode, serialize(elementType))
 impl SigmaSerializable for SType {
     fn sigma_serialize<W: SigmaByteWrite>(&self, w: &mut W) -> SigmaSerializeResult {
         // for reference see http://github.com/ScorexFoundation/sigmastate-interpreter/blob/25251c1313b0131835f92099f02cef8a5d932b5e/sigmastate/src/main/scala/sigmastate/serialization/TypeSerializer.scala#L25-L25
