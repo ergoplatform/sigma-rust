23585527076699b9ecd2f8d57c82e4b1a6cc0fab
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-08-23 15:35:08 +0300

    fix doc comment;

--- bindings/ergo-lib-wasm/src/transaction.rs
+++ bindings/ergo-lib-wasm/src/transaction.rs
@@ -183,7 +183,7 @@ impl UnsignedTransaction {
     }
 
     /// JSON representation according to EIP-12 https://github.com/ergoplatform/eips/pull/23
-    /// (similar to [`Self::to_json`], but with box value and token amount encoding as strings)
+    /// (similar to [`Self::to_json`], but as JS object with box value and token amount encoding as strings)
     pub fn to_js_eip12(&self) -> Result<JsValue, JsValue> {
         let tx_dapp: UnsignedTransactionJsonEip12 = self.0.clone().into();
         JsValue::from_serde(&tx_dapp).map_err(|e| JsValue::from_str(&format!("{}", e)))
