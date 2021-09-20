1291a689e2ec71d05bc8f41ada94a86549d6ca6c
ross-weir <29697678+ross-weir@users.noreply.github.com>
2021-09-20 20:28:20 +1000

    tests: add eval_self_box_index

--- ergotree-interpreter/src/eval/scontext.rs
+++ ergotree-interpreter/src/eval/scontext.rs
@@ -40,3 +40,41 @@ pub(crate) static SELF_BOX_INDEX_EVAL_FN: EvalFn = |_env, ctx, obj, _args| {
         .ok_or_else(|| EvalError::NotFound("Context.selfBoxIndex: box not found".to_string()))?;
     Ok(Value::Int(box_index as i32))
 };
+
+#[cfg(test)]
+#[cfg(feature = "arbitrary")]
+mod tests {
+    use crate::eval::context::ir_ergo_box_dummy::IrErgoBoxDummy;
+    use crate::eval::context::Context;
+    use crate::eval::tests::eval_out;
+    use ergotree_ir::mir::expr::Expr;
+    use ergotree_ir::mir::property_call::PropertyCall;
+    use ergotree_ir::types::scontext;
+    use sigma_test_util::force_any_val;
+    use std::rc::Rc;
+
+    fn make_ctx_inputs_includes_self_box() -> Context {
+        let ctx = force_any_val::<Context>();
+        let self_box = force_any_val::<IrErgoBoxDummy>();
+        let inputs = vec![
+            force_any_val::<IrErgoBoxDummy>().id.clone(),
+            self_box.id.clone(),
+        ];
+        Context {
+            height: 0u32,
+            self_box: self_box.id.clone(),
+            inputs,
+            ..ctx
+        }
+    }
+
+    #[test]
+    fn eval_self_box_index() {
+        let expr: Expr =
+            PropertyCall::new(Expr::Context, scontext::SELF_BOX_INDEX_PROPERTY.clone())
+                .unwrap()
+                .into();
+        let rc = Rc::new(make_ctx_inputs_includes_self_box());
+        assert_eq!(eval_out::<i32>(&expr, rc.clone()), 1);
+    }
+}
