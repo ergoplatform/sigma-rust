8b9ec08c069ab14414a039b763ae1be19d302faf
Timothy Ling <tim.ling@gmail.com>
2021-09-24 13:05:32 +1000

    Move `SubstConst` internals to `ergotree-ir::ergo_tree`
    Also undo some prior leakage of internal code.

--- ergotree-interpreter/src/eval/subst_const.rs
+++ ergotree-interpreter/src/eval/subst_const.rs
@@ -2,27 +2,16 @@ use crate::eval::env::Env;
 use crate::eval::EvalContext;
 use crate::eval::EvalError;
 use crate::eval::Evaluable;
-use ergotree_ir::ergo_tree::ErgoTreeHeader;
-use ergotree_ir::ergo_tree::ErgoTreeVersion;
+use ergotree_ir::ergo_tree::substitute_constants;
 use ergotree_ir::mir::constant::Constant;
 use ergotree_ir::mir::subst_const::SubstConstants;
 use ergotree_ir::mir::value::CollKind;
 use ergotree_ir::mir::value::NativeColl;
 use ergotree_ir::mir::value::Value;
-use ergotree_ir::serialization::constant_store::ConstantStore;
-use ergotree_ir::serialization::sigma_byte_reader::SigmaByteReader;
-use ergotree_ir::serialization::sigma_byte_writer::SigmaByteWriter;
-use ergotree_ir::serialization::SigmaParsingError;
-use ergotree_ir::serialization::SigmaSerializable;
-use ergotree_ir::serialization::SigmaSerializationError;
 use ergotree_ir::types::stype::SType;
 use ergotree_ir::util::AsVecI8;
 use ergotree_ir::util::AsVecU8;
-use sigma_ser::vlq_encode::ReadSigmaVlqExt;
-use sigma_ser::vlq_encode::WriteSigmaVlqExt;
 use std::convert::TryFrom;
-use std::io::Cursor;
-use std::io::Read;
 
 impl Evaluable for SubstConstants {
     fn eval(&self, env: &Env, ctx: &mut EvalContext) -> Result<Value, EvalError> {
@@ -73,76 +62,9 @@ impl Evaluable for SubstConstants {
             };
 
         if let Value::Coll(CollKind::NativeColl(NativeColl::CollByte(b))) = script_bytes_v {
-            let mut data = Vec::new();
-            let mut w = SigmaByteWriter::new(&mut data, None);
-            let cursor = Cursor::new(b.as_vec_u8());
-            let mut r = SigmaByteReader::new(cursor, ConstantStore::empty());
-
-            // Check the header
-            let header = ErgoTreeHeader::sigma_parse(&mut r).map_err(map_parsing_err)?;
-            if header.version() != ErgoTreeVersion::V0 && !header.has_size() {
-                return Err(EvalError::Misc(format!(
-                    "Invalid ErgoTreeHeader, size bit is expected for version {:?}",
-                    header.version()
-                )));
-            }
-            if header.has_size() {
-                r.get_u32().map_err(map_serialization_err)?;
-            }
-
-            // Deserialize the constants within the script
-            let constants = if header.is_constant_segregation() {
-                let num_consts = r.get_u32().map_err(map_serialization_err)?;
-
-                if num_consts > 0 {
-                    let mut res = Vec::with_capacity(num_consts as usize);
-                    for _ in 0..num_consts {
-                        res.push(
-                            Constant::sigma_parse(&mut r).map_err(EvalError::SigmaParsingError)?,
-                        );
-                    }
-                    Some(res)
-                } else {
-                    None
-                }
-            } else {
-                None
-            };
-
-            // Now capture the remaining tree bytes
-            let mut buf = vec![0_u8; b.len()];
-            let num_tree_bytes = r
-                .read_to_end(&mut buf)
-                .map_err(|e| EvalError::Misc(format!("{}", e)))?;
-            let constants_len = if let Some(c) = constants.as_ref() {
-                c.len() as u32
-            } else {
-                0
-            };
-            header
-                .sigma_serialize(&mut w)
-                .map_err(map_serialization_err)?;
-
-            // TODO from sigmastate: don't serialize the following when segregation is off
-            w.put_u32(constants_len).map_err(map_serialization_err)?;
-
-            // Substitute constants
-            if let Some(constants) = constants {
-                for (i, c) in constants.iter().enumerate() {
-                    if let Some(ix) = positions.iter().position(|j| *j == i) {
-                        if c.tpe == new_constants[ix].tpe && c.tpe == new_constants_type {
-                            new_constants[ix].sigma_serialize(&mut w)?;
-                        }
-                    } else {
-                        // No substitution
-                        c.sigma_serialize(&mut w)
-                            .map_err(EvalError::SigmaSerializationError)?;
-                    }
-                }
-            }
-
-            // Extend with unmodified tree bytes.
-            data.extend_from_slice(&buf[0..num_tree_bytes]);
+            let data =
+                substitute_constants(b.as_vec_u8(), positions, new_constants, new_constants_type)
+                    .map_err(|e| EvalError::Misc(format!("{:?}", e)))?;
             Ok(Value::Coll(CollKind::NativeColl(NativeColl::CollByte(
                 data.as_vec_i8(),
             ))))
@@ -155,14 +77,6 @@ impl Evaluable for SubstConstants {
     }
 }
 
-fn map_parsing_err(io_err: std::io::Error) -> EvalError {
-    EvalError::SigmaParsingError(SigmaParsingError::Io(format!("{}", io_err)))
-}
-
-fn map_serialization_err<T: std::fmt::Debug>(e: T) -> EvalError {
-    EvalError::SigmaSerializationError(SigmaSerializationError::Io(format!("{:?}", e)))
-}
-
 #[cfg(test)]
 //#[cfg(feature = "arbitrary")]
 #[allow(clippy::unwrap_used)]
@@ -170,8 +84,9 @@ fn map_serialization_err<T: std::fmt::Debug>(e: T) -> EvalError {
 #[allow(clippy::expect_used)]
 mod tests {
     use ergotree_ir::{
-        ergo_tree::ErgoTree,
+        ergo_tree::{ErgoTree, ErgoTreeHeader},
         mir::{constant::Literal, expr::Expr},
+        serialization::SigmaSerializable,
     };
 
     use crate::eval::tests::try_eval_out_wo_ctx;
