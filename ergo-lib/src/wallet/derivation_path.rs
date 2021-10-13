a178900464852f71c39a6c9e15481a31cb219af0
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-10-13 10:21:40 +0300

    DerivationPath rough implementaion;

--- ergo-lib/src/wallet/derivation_path.rs
+++ ergo-lib/src/wallet/derivation_path.rs
@@ -1,32 +1,77 @@
-pub struct Index(u32);
+//! Derivation path according to
+//! BIP-44 <https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki>
+//! and EIP-3 <https://github.com/ergoplatform/eips/blob/master/eip-0003.md>
 
-impl Index {
-    pub const HARD_RANGE_START: u32 = 0x80000000;
+/// Index for hardened derivation
+pub struct ChildIndexHardened(u32);
+/// Index for normal(non-hardened) derivation
+pub struct ChildIndexNormal(u32);
 
-    pub fn hardened(i: u32) -> Self {
-        Self(i | Self::HARD_RANGE_START)
-    }
-
-    pub fn is_hardened(&self) -> bool {
-        (self.0 & Self::HARD_RANGE_START) != 0
-    }
+/// Child index for derivation
+pub enum ChildIndex {
+    /// Index for hardened derivation
+    Hardened(ChildIndexHardened),
+    /// Index for normal(non-hardened) derivation
+    Normal(ChildIndexNormal),
 }
 
-pub struct DerivationPath {
-    public_branch: bool,
-    decoded_path: Vec<Index>,
+const PURPOSE: ChildIndex = ChildIndex::Hardened(ChildIndexHardened(44));
+const ERG: ChildIndex = ChildIndex::Hardened(ChildIndexHardened(429));
+/// According to EIP-3 change is always 0 (external address)
+const CHANGE: ChildIndex = ChildIndex::Normal(ChildIndexNormal(0));
+const ZERO: ChildIndex = ChildIndex::Normal(ChildIndexNormal(0));
+
+/// Child index related errors
+pub enum ChildIndexError {
+    /// Nomber is too large
+    NumberTooLarge(u32),
 }
 
-impl DerivationPath {
-    pub fn from_acc_num(acc_num: u32) -> Self {
-        Self {
-            decoded_path: vec![44, 429, acc_num, 0, 0],
+impl ChildIndex {
+    /// Create an index for normal (non-hardened) derivation
+    pub fn normal(i: u32) -> Result<Self, ChildIndexError> {
+        if i & (1 << 31) == 0 {
+            Ok(ChildIndex::Normal(ChildIndexNormal(i)))
+        } else {
+            Err(ChildIndexError::NumberTooLarge(i))
         }
     }
 
-    /// Change bool
-    pub fn new(acc_num: u32, index: u32) -> Self {
-        todo!()
+    /// Create an index for hardened derivation
+    pub fn hardened(i: u32) -> Result<Self, ChildIndexError> {
+        if i & (1 << 31) == 0 {
+            Ok(ChildIndex::Hardened(ChildIndexHardened(i)))
+        } else {
+            Err(ChildIndexError::NumberTooLarge(i))
+        }
+    }
+}
+
+/// According to
+/// BIP-44 <https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki>
+/// and EIP-3 <https://github.com/ergoplatform/eips/blob/master/eip-0003.md>
+pub struct DerivationPath(Box<[ChildIndex]>);
+
+impl DerivationPath {
+    /// Create derivation path for a given account index (hardened)
+    /// m / 44' / 429' / acc' / 0 / 0
+    pub fn from_acc_num(acc: ChildIndexHardened) -> Self {
+        Self([PURPOSE, ERG, ChildIndex::Hardened(acc), CHANGE, ZERO].into())
+    }
+
+    /// Create derivation path for a given account index (hardened) and address index
+    /// m / 44' / 429' / acc' / 0 / address
+    pub fn new(acc: ChildIndexHardened, address: ChildIndexNormal) -> Self {
+        Self(
+            [
+                PURPOSE,
+                ERG,
+                ChildIndex::Hardened(acc),
+                CHANGE,
+                ChildIndex::Normal(address),
+            ]
+            .into(),
+        )
     }
 
     /// For 0x21 Sign Transaction command of Ergo Ledger App Protocol
--- ergo-lib/src/wallet/ext_pub_key.rs
+++ ergo-lib/src/wallet/ext_pub_key.rs
@@ -2,13 +2,20 @@ use ergotree_ir::chain::address::Address;
 
 use super::derivation_path::DerivationPath;
 
+pub type PubKey = [u8; 64];
+pub type ChainCode = [u8; 32];
+
 pub struct ExtPubKey {
-    pub public_key: [u8; 64],
-    chain_code: [u8; 32],
+    pub public_key: PubKey,
+    chain_code: ChainCode,
     pub derivation_path: DerivationPath,
 }
 
 impl ExtPubKey {
+    pub fn new(public_key: PubKey, chain_code: ChainCode) -> Self {
+        todo!()
+    }
+
     pub fn soft_derive(&self, index: u32) -> Self {
         todo!()
     }
