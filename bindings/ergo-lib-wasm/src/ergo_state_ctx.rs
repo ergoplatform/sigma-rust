16b23557bff77eddc0c4463aa90e87a69b4330a9
ross-weir <29697678+ross-weir@users.noreply.github.com>
2021-10-14 20:52:18 +1100

    task: wasm ErgoStateContext from headers

--- bindings/ergo-lib-wasm/src/ergo_state_ctx.rs
+++ bindings/ergo-lib-wasm/src/ergo_state_ctx.rs
@@ -7,6 +7,11 @@ use derive_more::{From, Into};
 
 use crate::header::PreHeader;
 
+/// Fixed number of last block headers in descending order (first header is the newest one)
+#[wasm_bindgen]
+#[derive(From, Into)]
+pub struct ErgoStateContextHeaders(chain::ergo_state_context::ErgoStateContextHeaders);
+
 /// Blockchain state (last headers, etc.)
 #[wasm_bindgen]
 #[derive(PartialEq, Eq, Debug, Clone, From, Into)]
@@ -16,14 +21,9 @@ pub struct ErgoStateContext(chain::ergo_state_context::ErgoStateContext);
 impl ErgoStateContext {
     /// Create new context from pre-header
     #[wasm_bindgen(constructor)]
-    pub fn new(pre_header: PreHeader) -> Self {
-        let mut ergo_state_context = chain::ergo_state_context::ErgoStateContext::dummy();
-        ergo_state_context.pre_header = pre_header.into();
+    pub fn new(pre_header: PreHeader, headers: ErgoStateContextHeaders) -> Self {
+        let ergo_state_context =
+            chain::ergo_state_context::ErgoStateContext::new(pre_header.into(), headers.into());
         ergo_state_context.into()
-    }
-
-    /// empty (dummy) context (for signing P2PK tx only)
-    pub fn dummy() -> ErgoStateContext {
-        ErgoStateContext(chain::ergo_state_context::ErgoStateContext::dummy())
     }
 }
