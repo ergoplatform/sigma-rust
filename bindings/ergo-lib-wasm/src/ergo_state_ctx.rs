699c85984b2d4138c8340ef85218115e5027bc66
ross-weir <29697678+ross-weir@users.noreply.github.com>
2021-10-20 19:33:52 +1100

    fix: check header length in wasm ergo state context

--- bindings/ergo-lib-wasm/src/ergo_state_ctx.rs
+++ bindings/ergo-lib-wasm/src/ergo_state_ctx.rs
@@ -17,9 +17,18 @@ pub struct ErgoStateContext(chain::ergo_state_context::ErgoStateContext);
 impl ErgoStateContext {
     /// Create new context from pre-header
     #[wasm_bindgen(constructor)]
-    pub fn new(pre_header: PreHeader, headers: BlockHeaders) -> Self {
-        let ergo_state_context =
-            chain::ergo_state_context::ErgoStateContext::new(pre_header.into(), headers.into());
-        ergo_state_context.into()
+    pub fn new(pre_header: PreHeader, headers: BlockHeaders) -> Result<ErgoStateContext, JsValue> {
+        match headers.len() {
+            10 => Ok(chain::ergo_state_context::ErgoStateContext::new(
+                pre_header.into(),
+                headers.into(),
+            )
+            .into()),
+            h => Err(js_sys::Error::new(&format!(
+                "Wrong header length, expected 10 but got {}",
+                h
+            ))
+            .into()),
+        }
     }
 }
