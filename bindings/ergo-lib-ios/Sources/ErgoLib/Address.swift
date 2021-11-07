f2d3580fe0203f63efd56df5d5d2af398d7d7acf
Timothy Ling <tim.ling@gmail.com>
2021-11-07 16:24:48 +1100

    Fix Address.swift

--- bindings/ergo-lib-ios/Sources/ErgoLib/Address.swift
+++ bindings/ergo-lib-ios/Sources/ErgoLib/Address.swift
@@ -1,8 +1,62 @@
-//
-//  File.swift
-//  
-//
-//  Created by Timothy Ling on 7/11/21.
-//
-
 import Foundation
+import ErgoLibC
+
+class Address {
+    internal var pointer: AddressPtr
+
+    init(withTestnetAddress addressStr: String) throws {
+        self.pointer = try Address.fromTestnetAddress(addressStr: addressStr)
+    }
+
+    init(withMainnetAddress addressStr: String) throws {
+        self.pointer = try Address.fromMainnetAddress(addressStr: addressStr)
+    }
+    
+    init(withBase58Address addressStr: String) throws {
+        self.pointer = try Address.fromBase58(addressStr: addressStr)
+    }
+    
+    func toBase58(networkPrefix: NetworkPrefix) throws -> String {
+        var cStr: UnsafePointer<CChar>?
+        let error = ergo_wallet_address_to_base58(self.pointer, networkPrefix.rawValue, &cStr)
+        try checkError(error)
+        let str = String(cString: cStr!)
+        ergo_wallet_delete_string(UnsafeMutablePointer(mutating: cStr))
+        return str
+    }
+    
+    private static func fromTestnetAddress(addressStr: String) throws -> AddressPtr {
+        var ptr: AddressPtr?
+        let error = addressStr.withCString { cs in
+            ergo_wallet_address_from_testnet(cs, &ptr)
+        }
+        try checkError(error)
+        return ptr!
+    }
+    
+    private static func fromMainnetAddress(addressStr: String) throws -> AddressPtr {
+        var ptr: AddressPtr?
+        let error = addressStr.withCString { cs in
+            ergo_wallet_address_from_mainnet(cs, &ptr)
+        }
+        try checkError(error)
+        return ptr!
+    }
+    
+    private static func fromBase58(addressStr: String) throws -> AddressPtr {
+        var ptr: AddressPtr?
+        let error = addressStr.withCString { cs in
+            ergo_wallet_address_from_base58(cs, &ptr)
+        }
+        try checkError(error)
+        return ptr!
+    }
+    
+    deinit {
+        ergo_wallet_address_delete(self.pointer)
+    }
+}
+enum NetworkPrefix: UInt8 {
+    case Mainnet = 0
+    case Testnet = 16
+}
--- bindings/ergo-lib-ios/Sources/ErgoLib/ErgoLib.swift
+++ bindings/ergo-lib-ios/Sources/ErgoLib/ErgoLib.swift
@@ -26,7 +26,7 @@ class UnspentBoxes {
     }
 }
 
-private func checkError(_ error: ErrorPtr?) throws {
+internal func checkError(_ error: ErrorPtr?) throws {
     if error == nil {
         return
     }
@@ -80,61 +80,6 @@ class ErgoStateContext {
     }
 }
 
-class Address {
-    internal var pointer: AddressPtr
-
-    init(withTestnetAddress addressStr: String) throws {
-        self.pointer = try Address.fromTestnetAddress(addressStr: addressStr)
-    }
-
-    init(withMainnetAddress addressStr: String) throws {
-        self.pointer = try Address.fromMainnetAddress(addressStr: addressStr)
-    }
-    
-    init(withBase58Address addressStr: String) throws {
-        self.pointer = try Address.fromBase58(addressStr: addressStr)
-    }
-    
-    private static func fromTestnetAddress(addressStr: String) throws -> AddressPtr {
-        var ptr: AddressPtr?
-        let error = addressStr.withCString { cs in
-            ergo_wallet_address_from_testnet(cs, &ptr)
-        }
-        try checkError(error)
-        return ptr!
-    }
-    
-    private static func fromMainnetAddress(addressStr: String) throws -> AddressPtr {
-        var ptr: AddressPtr?
-        let error = addressStr.withCString { cs in
-            ergo_wallet_address_from_mainnet(cs, &ptr)
-        }
-        try checkError(error)
-        return ptr!
-    }
-    
-    private static func fromBase58(addressStr: String) throws -> AddressPtr {
-        var ptr: AddressPtr?
-        let error = addressStr.withCString { cs in
-            ergo_wallet_address_from_base58(cs, &ptr)
-        }
-        try checkError(error)
-        return ptr!
-    }
-    
-    func toBase58(networkPrefix: NetworkPrefix) throws -> String {
-        var cStr: UnsafePointer<CChar>?
-        let error = ergo_wallet_address_to_base58(self.pointer, networkPrefix.rawValue, &cStr)
-        try checkError(error)
-        let str = String(cString: cStr!)
-        ergo_wallet_delete_string(UnsafeMutablePointer(mutating: cStr))
-        return str
-    }
-    
-    deinit {
-        ergo_wallet_address_delete(self.pointer)
-    }
-}
 
 class ErgoBoxCandidate {
     internal var pointer: ErgoBoxCandidatePtr
@@ -207,7 +152,3 @@ class Wallet {
     }
 }
 
-enum NetworkPrefix: UInt8 {
-    case Mainnet = 0
-    case Testnet = 16
-}
