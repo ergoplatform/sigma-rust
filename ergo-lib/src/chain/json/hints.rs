efa4e1249fb13e6254cbafabdf2a751ed1be7941
sahandzou <sahandzoufan79@gmail.com>
2021-11-17 01:33:55 +0330

    commitment hint json format same as node

--- ergo-lib/src/chain/json/hints.rs
+++ ergo-lib/src/chain/json/hints.rs
@@ -1,6 +1,6 @@
 use k256::{FieldBytes, Scalar};
 use serde::{Deserialize, Serialize};
-use crate::ergotree_interpreter::sigma_protocol::prover::hint::{OwnCommitment, SimulatedCommitment};
+use crate::ergotree_interpreter::sigma_protocol::prover::hint::{CommitmentHint, OwnCommitment, SimulatedCommitment};
 use num_bigint::BigUint;
 use ergotree_interpreter::sigma_protocol::prover::hint::RealCommitment;
 use ergotree_ir::chain::base16_bytes::{Base16DecodedBytes, Base16EncodedBytes};
@@ -24,16 +24,84 @@ pub struct OwnCommitmentJson {
     pub image:String,
 }
 
+#[derive(Serialize,Deserialize)]
 pub struct RealCommitmentJson{
     pub image:String,
     pub position:String,
     pub a:String,
 }
 
+#[derive(Serialize,Deserialize)]
 pub struct SimulatedCommitmentJson{
     pub image:String,
     pub position:String,
     pub a:String,
+}
+
+#[derive(Serialize,Deserialize)]
+pub struct PublicKeyJson{
+    pub op:i32,
+    pub h:String,
+}
+
+#[derive(Serialize,Deserialize)]
+pub struct CommitmentHintJson{
+    pub hint:String,
+    pub pubkey:PublicKeyJson,
+    pub position:String,
+    #[serde(rename = "type")]
+    pub proof_type:String,
+    pub a:String,
+    pub secret:Option<String>,
+}
+
+// todo trait should be implemented to avoid from duplicated code
+impl From<CommitmentHint> for CommitmentHintJson{
+    fn from(v:CommitmentHint) -> Self{
+        let mut hint:Option<String>=None;
+        let mut secret:Option<String>=None;
+        let mut a:Option<String>=None;
+        let proof_type="dlog".to_string();
+        let mut position:Option<String>=None;
+        let mut ec_point:Option<String>=None;
+        match v{
+            CommitmentHint::OwnCommitment(cmt) => {
+                hint=Some("cmtWithSecret".to_string());
+                secret=Some(hex::encode(cmt.secret_randomness.clone().to_bytes().as_slice()));
+                a=Some(hex::encode(cmt.commitment.clone().bytes().as_slice()));
+                position=Some(cmt.position.positions.clone().into_iter().map(|d| std::char::from_digit(d as u32,10).unwrap().to_string()).collect::<Vec<_>>().join("-"));
+                ec_point=Some(hex::encode(cmt.image.clone().sigma_serialize_bytes().unwrap().as_slice())[2..].to_string());
+
+            }
+            CommitmentHint::RealCommitment(cmt) => {
+                hint=Some("cmtReal".to_string());
+                a=Some(hex::encode(cmt.commitment.clone().bytes().as_slice()));
+                position=Some(cmt.position.positions.clone().into_iter().map(|d| std::char::from_digit(d as u32,10).unwrap().to_string()).collect::<Vec<_>>().join("-"));
+                ec_point=Some(hex::encode(cmt.image.clone().sigma_serialize_bytes().unwrap().as_slice())[2..].to_string());
+
+            }
+            CommitmentHint::SimulatedCommitment(cmt) => {
+                hint=Some("cmtSimulated".to_string());
+                a=Some(hex::encode(cmt.commitment.clone().bytes().as_slice()));
+                position=Some(cmt.position.positions.clone().into_iter().map(|d| std::char::from_digit(d as u32,10).unwrap().to_string()).collect::<Vec<_>>().join("-"));
+                ec_point=Some(hex::encode(cmt.image.clone().sigma_serialize_bytes().unwrap().as_slice())[2..].to_string());
+
+            }
+        }
+        let public_key=PublicKeyJson{
+            op:-51,
+            h:ec_point.unwrap(),
+        };
+
+        CommitmentHintJson{
+            hint:hint.unwrap(),
+            pubkey:public_key,
+            position:position.unwrap(),
+            proof_type,
+            a:a.unwrap(),
+            secret,
+        }
+    }
 }
 
 impl From<OwnCommitment> for OwnCommitmentJson {
@@ -107,7 +175,8 @@ impl From<SimulatedCommitmentJson> for SimulatedCommitment{
 }
 #[cfg(test)]
 mod tests{
-    use crate::chain::json::hints::OwnCommitmentJson;
+    use ergotree_interpreter::sigma_protocol::prover::hint::CommitmentHint;
+    use crate::chain::json::hints::{CommitmentHintJson, OwnCommitmentJson};
     use crate::ergotree_interpreter::sigma_protocol::dlog_protocol::interactive_prover;
     use crate::ergotree_interpreter::sigma_protocol::FirstProverMessage;
     use crate::ergotree_interpreter::sigma_protocol::private_input::DlogProverInput;
@@ -137,6 +206,26 @@ mod tests{
         assert_eq!(own_com.image.clone(),own_commitment.image.clone());
         assert_eq!(own_com.position.clone(),own_commitment.position.clone());
         assert_eq!(own_com.commitment.clone(),own_commitment.commitment.clone());
+    }
+
+    #[test]
+    fn commitmetn_hint_node_format(){
+        let secret1 = DlogProverInput::random();
+        let pk1 = secret1.public_image();
+        let (r, a) = interactive_prover::first_message();
+        let own_commitment= OwnCommitment
+        {
+            image: SigmaBoolean::ProofOfKnowledge(SigmaProofOfKnowledgeTree::ProveDlog(pk1.clone())),
+            secret_randomness: r,
+            commitment: FirstProverMessage::FirstDlogProverMessage(
+                a.clone()
+            ),
+            position: NodePosition::crypto_tree_prefix().clone(),
+        };
+
+        let json:CommitmentHintJson=CommitmentHintJson::from(CommitmentHint::OwnCommitment(own_commitment.clone()));
+        println!("{}",serde_json::to_string(&json).unwrap());
+
     }
 }
 
