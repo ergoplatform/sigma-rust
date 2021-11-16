7cff5a23cc2361026053bbfd5540b4c1bf8ac287
sahandzou <sahandzoufan79@gmail.com>
2021-11-16 14:17:26 +0330

    commitments json wrapper done

--- ergo-lib/src/chain/json/hints.rs
+++ ergo-lib/src/chain/json/hints.rs
@@ -1,6 +1,6 @@
 use k256::{FieldBytes, Scalar};
 use serde::{Deserialize, Serialize};
-use crate::ergotree_interpreter::sigma_protocol::prover::hint::OwnCommitment;
+use crate::ergotree_interpreter::sigma_protocol::prover::hint::{OwnCommitment, SimulatedCommitment};
 use num_bigint::BigUint;
 use ergotree_interpreter::sigma_protocol::prover::hint::RealCommitment;
 use ergotree_ir::chain::base16_bytes::{Base16DecodedBytes, Base16EncodedBytes};
@@ -29,31 +29,22 @@ pub struct RealCommitmentJson{
     pub position:String,
     pub a:String,
 }
-// impl Serialize for Scalar{
-//     fn serialize_field<T: ?Sized>(
-//       &mut self,
-//       key: &'static str,
-//       value: &T,
-//     ) -> Result<(), Self::Error>
-//         where
-//             T: Serialize;
-//
-// }
+
+pub struct SimulatedCommitmentJson{
+    pub image:String,
+    pub position:String,
+    pub a:String,
+}
 
 impl From<OwnCommitment> for OwnCommitmentJson {
     fn from(v: OwnCommitment) -> Self {
         let ec_point=&hex::encode(v.image.clone().sigma_serialize_bytes().unwrap().as_slice())[2..].to_string();
 
         OwnCommitmentJson {
-            // secret:Base16EncodedBytes::new(v.secret_randomness.to_bytes().as_slice()).0,
             secret:hex::encode(v.secret_randomness.clone().to_bytes().as_slice()),
-            // secret:v.secret_randomness.clone(),
-            // secret:BigUint::from_bytes_be(v.secret_randomness.to_bytes().as_slice()).to_str_radix(10),
             position:v.position.positions.clone().into_iter().map(|d| std::char::from_digit(d as u32,10).unwrap()).collect(),
             a:hex::encode(v.commitment.clone().bytes().as_slice()),
             image:ec_point.clone(),
-
-            // image:hex::encode(v.image.clone().sigma_serialize_bytes().unwrap().as_slice()),
         }
     }
 }
@@ -62,23 +53,29 @@ impl From<RealCommitment> for RealCommitmentJson{
     fn from(v: RealCommitment) -> Self {
         let ec_point=&hex::encode(v.image.clone().sigma_serialize_bytes().unwrap().as_slice())[2..].to_string();
         RealCommitmentJson {
-
             position:v.position.positions.clone().into_iter().map(|d| std::char::from_digit(d as u32,10).unwrap()).collect(),
             a:hex::encode(v.commitment.clone().bytes().as_slice()),
             image:ec_point.clone(),
-            // image:SigmaBoolean::ProofOfKnowledge(SigmaProofOfKnowledgeTree::ProveDlog::new(v.image))
         }
     }
 }
 
+impl From<SimulatedCommitment> for SimulatedCommitmentJson{
+    fn from(v: SimulatedCommitment) -> Self {
+        let ec_point=&hex::encode(v.image.clone().sigma_serialize_bytes().unwrap().as_slice())[2..].to_string();
+        SimulatedCommitmentJson {
+            position:v.position.positions.clone().into_iter().map(|d| std::char::from_digit(d as u32,10).unwrap()).collect(),
+            a:hex::encode(v.commitment.clone().bytes().as_slice()),
+            image:ec_point.clone(),
+        }
+    }
+}
+
+
 impl From<OwnCommitmentJson> for OwnCommitment{
     fn from(v:OwnCommitmentJson)->Self{
-        // println!("{}",v.image.clone());
-        // println!("{}",EcPoint::from_base16_str("02a65551523e09530bc8de55d426ff60aace7388cef08f5477c8726713f68c0e78".to_string()).unwrap().sigma_serialize_bytes().unwrap().as_slice().to_vec()[0]);
         OwnCommitment{
             image:SigmaBoolean::ProofOfKnowledge(SigmaProofOfKnowledgeTree::ProveDlog(OtherProveDlog::from(EcPoint::from_base16_str(v.image.clone()).unwrap()))),
-            // secret_randomness:Scalar(ScalarImpl::from_bytes_reduced(hex::decode(v.secret.clone()).unwrap().as_slice())),
-            // secret_randomness:Scalar::from(GroupSizedBytes(hex::decode(v.secret.clone()).unwrap().as_slice().into())),
             secret_randomness:Scalar::from_bytes_reduced(hex::decode(v.secret.clone()).unwrap().as_slice().into()),
             position:NodePosition{positions:v.position.clone().chars().map(|chr| chr.to_digit(10).unwrap() as usize).collect()},
             commitment:FirstProverMessage::FirstDlogProverMessage(FirstDlogProverMessage::from(EcPoint::from_base16_str(v.a.clone()).unwrap())),
@@ -86,4 +83,60 @@ impl From<OwnCommitmentJson> for OwnCommitment{
 
     }
 }
-// impl From<>
+
+impl From<RealCommitmentJson> for RealCommitment{
+    fn from(v:RealCommitmentJson)->Self{
+        RealCommitment{
+            image:SigmaBoolean::ProofOfKnowledge(SigmaProofOfKnowledgeTree::ProveDlog(OtherProveDlog::from(EcPoint::from_base16_str(v.image.clone()).unwrap()))),
+            position:NodePosition{positions:v.position.clone().chars().map(|chr| chr.to_digit(10).unwrap() as usize).collect()},
+            commitment:FirstProverMessage::FirstDlogProverMessage(FirstDlogProverMessage::from(EcPoint::from_base16_str(v.a.clone()).unwrap())),
+        }
+
+    }
+}
+
+impl From<SimulatedCommitmentJson> for SimulatedCommitment{
+    fn from(v:SimulatedCommitmentJson)->Self{
+        SimulatedCommitment{
+            image:SigmaBoolean::ProofOfKnowledge(SigmaProofOfKnowledgeTree::ProveDlog(OtherProveDlog::from(EcPoint::from_base16_str(v.image.clone()).unwrap()))),
+            position:NodePosition{positions:v.position.clone().chars().map(|chr| chr.to_digit(10).unwrap() as usize).collect()},
+            commitment:FirstProverMessage::FirstDlogProverMessage(FirstDlogProverMessage::from(EcPoint::from_base16_str(v.a.clone()).unwrap())),
+        }
+
+    }
+}
+#[cfg(test)]
+mod tests{
+    use crate::chain::json::hints::OwnCommitmentJson;
+    use crate::ergotree_interpreter::sigma_protocol::dlog_protocol::interactive_prover;
+    use crate::ergotree_interpreter::sigma_protocol::FirstProverMessage;
+    use crate::ergotree_interpreter::sigma_protocol::private_input::DlogProverInput;
+    use crate::ergotree_interpreter::sigma_protocol::unproven_tree::NodePosition;
+    use crate::ergotree_ir::sigma_protocol::sigma_boolean::{SigmaBoolean, SigmaProofOfKnowledgeTree};
+    use crate::ergotree_interpreter::sigma_protocol::prover::hint::{OwnCommitment, SimulatedCommitment};
+
+    #[test]
+    fn round_trip_own_commitment(){
+        let secret1 = DlogProverInput::random();
+        let pk1 = secret1.public_image();
+        let (r, a) = interactive_prover::first_message();
+        let own_commitment= OwnCommitment
+        {
+            image: SigmaBoolean::ProofOfKnowledge(SigmaProofOfKnowledgeTree::ProveDlog(pk1.clone())),
+            secret_randomness: r,
+            commitment: FirstProverMessage::FirstDlogProverMessage(
+                a.clone()
+            ),
+            position: NodePosition::crypto_tree_prefix().clone(),
+        };
+        let json:OwnCommitmentJson=OwnCommitmentJson::from(own_commitment.clone());
+        let reverse=serde_json::to_string(&json).unwrap();
+        let own_com_json:OwnCommitmentJson=serde_json::from_str(&reverse).unwrap();
+        let own_com:OwnCommitment=OwnCommitment::from(own_com_json);
+        assert_eq!(own_com.secret_randomness.clone(),own_commitment.secret_randomness.clone());
+        assert_eq!(own_com.image.clone(),own_commitment.image.clone());
+        assert_eq!(own_com.position.clone(),own_commitment.position.clone());
+        assert_eq!(own_com.commitment.clone(),own_commitment.commitment.clone());
+    }
+}
+
