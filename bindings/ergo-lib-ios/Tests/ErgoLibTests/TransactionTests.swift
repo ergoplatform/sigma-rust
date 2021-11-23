2ec4eae02e6f4b8501da9b601b1d12e335b00de5
Timothy Ling <tim.ling@gmail.com>
2021-11-23 11:59:39 +1100

    Add `TxId` test for swift binding

--- bindings/ergo-lib-ios/Tests/ErgoLibTests/TransactionTests.swift
+++ bindings/ergo-lib-ios/Tests/ErgoLibTests/TransactionTests.swift
@@ -4,6 +4,12 @@ import XCTest
 @testable import ErgoLibC
 
 final class TransactionTests: XCTestCase {
+    func testTxId() throws {
+        let str = "93d344aa527e18e5a221db060ea1a868f46b61e4537e6e5f69ecc40334c15e38"
+        let txId = try TxId(withString: str)
+        XCTAssertEqual(try txId.toString(), str)
+    }
+    
     func testUnsignedTransaction() throws {
     }
 }
