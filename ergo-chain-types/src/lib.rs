ff08e4e5e085e8e7587c0c83087aba60e69549ed
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-11-23 18:21:08 +0200

    add ergo-chain-types and ergo-nipopow crates;

---
+++ ergo-chain-types/src/lib.rs
@@ -0,0 +1,18 @@
+//! Ergo blockchain types
+
+// Coding conventions
+#![forbid(unsafe_code)]
+#![deny(non_upper_case_globals)]
+#![deny(non_camel_case_types)]
+#![deny(non_snake_case)]
+#![deny(unused_mut)]
+#![deny(dead_code)]
+#![deny(unused_imports)]
+#![deny(missing_docs)]
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(clippy::wildcard_enum_match_arm)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::todo)]
+#![deny(clippy::unimplemented)]
+#![deny(clippy::panic)]
