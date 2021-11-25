f675fdcaa93dd8164eb450d4210ee06b4dc2ea9e
ross-weir <29697678+ross-weir@users.noreply.github.com>
2021-11-26 07:58:16 +1100

    use encoding in seed tests

--- ergo-lib/src/wallet/mnemonic.rs
+++ ergo-lib/src/wallet/mnemonic.rs
@@ -41,27 +41,19 @@ mod tests {
     fn test_mnemonic_to_seed() {
         let mnemonic = "change me do not use me change me do not use me";
         let seed = Mnemonic::to_seed(mnemonic, "");
-        let expected: MnemonicSeed = [
-            197, 178, 83, 123, 82, 178, 123, 144, 59, 52, 196, 35, 120, 60, 237, 23, 196, 137, 228,
-            56, 94, 198, 212, 157, 106, 25, 167, 248, 146, 236, 211, 145, 125, 179, 102, 117, 222,
-            54, 188, 190, 59, 141, 188, 111, 128, 56, 119, 244, 21, 91, 223, 131, 72, 44, 165, 240,
-            252, 66, 130, 166, 26, 200, 66, 163,
-        ];
+        let encoded_seed = base16::encode_lower(&seed);
+        let expected = "c5b2537b52b27b903b34c423783ced17c489e4385ec6d49d6a19a7f892ecd3917db36675de36bcbe3b8dbc6f803877f4155bdf83482ca5f0fc4282a61ac842a3";
 
-        assert_eq!(&seed[..], expected);
+        assert_eq!(encoded_seed, expected);
     }
 
     #[test]
     fn test_mnemonic_to_seed_with_pass() {
         let mnemonic = "change me do not use me change me do not use me";
         let seed = Mnemonic::to_seed(mnemonic, "password123");
-        let expected: MnemonicSeed = [
-            223, 227, 8, 139, 136, 226, 235, 133, 136, 72, 46, 140, 86, 217, 205, 228, 151, 196,
-            225, 246, 63, 210, 155, 72, 12, 187, 14, 208, 34, 115, 49, 213, 19, 1, 207, 194, 212,
-            97, 172, 206, 100, 40, 104, 236, 182, 24, 163, 123, 79, 215, 93, 72, 220, 97, 137, 103,
-            76, 85, 251, 175, 216, 7, 214, 156,
-        ];
+        let encoded_seed = base16::encode_lower(&seed);
+        let expected = "dfe3088b88e2eb8588482e8c56d9cde497c4e1f63fd29b480cbb0ed0227331d51301cfc2d461acce642868ecb618a37b4fd75d48dc6189674c55fbafd807d69c";
 
-        assert_eq!(seed, expected);
+        assert_eq!(encoded_seed, expected);
     }
 }
