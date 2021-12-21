35f9314c187a795ed9b7ebccdccab06375ae9818
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-12-21 15:39:18 +0200

    fix audit.toml location, add yanked crates error;
    update k256 and elliptic_curve crates;

--- ergotree-interpreter/src/sigma_protocol.rs
+++ ergotree-interpreter/src/sigma_protocol.rs
@@ -18,11 +18,13 @@ mod sig_serializer;
 mod unchecked_tree;
 mod unproven_tree;
 
+use elliptic_curve::generic_array::GenericArray;
+use elliptic_curve::ops::Reduce;
 use ergotree_ir::sigma_protocol::sigma_boolean::SigmaBoolean;
 use k256::Scalar;
 
 use dlog_protocol::FirstDlogProverMessage;
-use std::convert::TryInto;
+use k256::U256;
 use unchecked_tree::UncheckedTree;
 use unproven_tree::{UnprovenLeaf, UnprovenSchnorr};
 
@@ -69,9 +71,7 @@ pub(crate) struct GroupSizedBytes(pub(crate) Box<[u8; GROUP_SIZE]>);
 impl From<GroupSizedBytes> for Scalar {
     fn from(b: GroupSizedBytes) -> Self {
         let sl: &[u8] = b.0.as_ref();
-        #[allow(clippy::unwrap_used)]
-        // since it's 32 bytes it's safe to unwrap
-        Scalar::from_bytes_reduced(sl.try_into().unwrap())
+        <Scalar as Reduce<U256>>::from_be_bytes_reduced(GenericArray::clone_from_slice(sl))
     }
 }
 
