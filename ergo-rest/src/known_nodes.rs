9f789cf07f846465ca3e0986fb41d48539a56c12
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-12-22 11:04:26 +0200

    fix doc comment links, silence linter errors;

--- ergo-rest/src/known_nodes.rs
+++ ergo-rest/src/known_nodes.rs
@@ -12,7 +12,7 @@ pub struct KnownNodes {
 
 impl KnownNodes {
     /// Load node addresses that serve REST API
-    pub fn load_from_addr(addrs: Vec<PeerAddr>) {
+    pub fn load_from_addr(_addrs: Vec<PeerAddr>) {
         todo!()
     }
 
@@ -21,16 +21,16 @@ impl KnownNodes {
         todo!()
     }
 
-    /// Load from the full nodes info (previously discovered and persisted from [`get_nodes`])
-    pub fn load_nodes(nodes: Vec<NodeInfo>) {
+    /// Load from the full nodes info (previously discovered and persisted from [`KnownNodes::get_nodes`])
+    pub fn load_nodes(_nodes: Vec<NodeInfo>) {
         todo!()
     }
 
     /// Ask known nodes for new peers until `target_new_discovered` new nodes are discovered
     pub async fn discover(
         &self,
-        max_parallel_req: usize,
-        target_new_discovered: usize,
+        _max_parallel_req: usize,
+        _target_new_discovered: usize,
     ) -> Result<(), NodeError> {
         todo!()
     }
