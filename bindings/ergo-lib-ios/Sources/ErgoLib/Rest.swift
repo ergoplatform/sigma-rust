079a8ec145a7da1cbc6c94b42a10e37b1e49b1c1
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-12-29 11:52:30 +0200

    draft async RestNode with get_info() method in Swift;

---
+++ bindings/ergo-lib-ios/Sources/ErgoLib/Rest.swift
@@ -0,0 +1,69 @@
+import Foundation
+import ErgoLibC
+
+class RestNode {
+    internal var pointer: RestApiRuntimePtr
+    
+    /// Create ergo node ``Rest`` API instance 
+    init() throws {
+        var ptr: RestApiRuntimePtr?
+        let error = ergo_lib_rest_api_runtime_create(&ptr)
+        try checkError(error)
+        self.pointer = ptr!
+    }
+    
+    /// GET on /info endpoint
+    func get_info(
+        nodeConf: NodeConf,
+        closure_success: @escaping (NodeInfo) -> Void
+        closure_fail: @escaping (String) -> Void
+    ) throws {
+        // step 1
+        let wrappedClosure = WrapClosure(closure: closure)
+        let userdata = Unmanaged.passRetained(wrappedClosure).toOpaque()
+
+        // step 2
+        let callback_success: @convention(c) (UnsafeMutableRawPointer, NodeInfoPtr) -> Void = { (_ userdata: UnsafeMutableRawPointer, _ nodeInfoPtr: NodeInfoPtr) in
+            let wrappedClosure: WrapClosure<(NodeInfo) -> Void> = Unmanaged.fromOpaque(userdata).takeRetainedValue()
+                    let nodeInfo = NodeInfo(withRawPointer: nodeInfoPtr)
+                    wrappedClosure.closure(nodeInfo)
+        }
+
+        // step 2
+        let callback_fail: @convention(c) (UnsafeMutableRawPointer, ErrorPtr) -> Void = { (_ userdata: UnsafeMutableRawPointer, _ errorPtr: ErrorPtr) in
+            let wrappedClosure: WrapClosure<(String) -> Void> = Unmanaged.fromOpaque(userdata).takeRetainedValue()
+                    let cStringReason = ergo_lib_error_to_string(errorPtr)
+                    let reason = String(cString: cStringReason!)
+                    ergo_lib_delete_string(cStringReason)
+                    ergo_lib_delete_error(error)
+                    wrappedClosure.closure(reason)
+        }
+
+        // step 3
+        let completion = CompletedCallback_NodeInfoPtr(userdata: userdata, callback_success: callback_success,
+            callback_fail: callback_fail)
+
+        let error = ergo_lib_rest_api_node_get_info(self.pointer, nodeConf.pointer, completion)
+
+        try checkError(error)
+    }
+    
+    deinit {
+        ergo_lib_rest_api_runtime_delete(self.pointer)
+    }
+}
+
+private class WrapClosure<T> {
+    fileprivate let closure: T
+    init(closure: T) {
+        self.closure = closure
+    }
+}
+
+class NodeInfo {
+    internal var pointer: NodeInfoPtr
+
+    internal init(withRawPointer ptr: NodeInfoPtr) {
+        self.pointer = ptr
+    }
+}
