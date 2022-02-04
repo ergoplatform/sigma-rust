9dcd203654aedb3bc5053c875a7c19bf8a237ff0
Timothy Ling <tim.ling@gmail.com>
2022-02-05 01:10:47 +1100

    Fix URL in WASM REST integration test

--- bindings/ergo-lib-wasm/tests/integration_test_rest_api.js
+++ bindings/ergo-lib-wasm/tests/integration_test_rest_api.js
@@ -5,7 +5,7 @@ import {
 } from '../pkg/ergo_lib_wasm';
 
 it('node REST API get_info endpoint', async () => {
-  let node_conf = new NodeConf("localhost:9053");
+  let node_conf = new NodeConf("http://localhost:9053");
   assert(node_conf != null);
   let res = await get_info(node_conf);
   assert(res != null);
