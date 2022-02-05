0c62408e15b646f041db87aab4f7be9054941100
Timothy Ling <tim.ling@gmail.com>
2022-02-06 01:16:09 +1100

    Bring in `node-fetch` dependency

--- bindings/ergo-lib-wasm/tests/integration_test_rest_api.js
+++ bindings/ergo-lib-wasm/tests/integration_test_rest_api.js
@@ -4,6 +4,17 @@ import {
   get_info, NodeConf,
 } from '../pkg/ergo_lib_wasm';
 
+import fetch from 'node-fetch';
+
+// @ts-ignore
+global.fetch = fetch;
+// @ts-ignore
+global.Headers = fetch.Headers;
+// @ts-ignore
+global.Request = fetch.Request;
+// @ts-ignore
+global.Response = fetch.Response;
+
 it('node REST API get_info endpoint', async () => {
   let node_conf = new NodeConf("127.0.0.1:9053");
   assert(node_conf != null);
