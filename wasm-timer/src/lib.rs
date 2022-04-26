1db0aee4c42349308a38856571c4a73218fb3060
Tim Ling <791016+kettlebell@users.noreply.github.com>
2022-04-26 16:07:40 +1000

    Disable clippy lints for `wasm-timer`

--- wasm-timer/src/lib.rs
+++ wasm-timer/src/lib.rs
@@ -17,7 +17,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
-
+#![allow(clippy::all)]
 pub use timer::*;
 
 #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
