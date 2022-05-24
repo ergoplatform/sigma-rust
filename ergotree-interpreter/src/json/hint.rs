beed77e06b642104e1040abfe3b75b6e26bf8003
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2022-05-24 09:55:30 +0300

    NodePosition json encoder;

--- ergotree-interpreter/src/json/hint.rs
+++ ergotree-interpreter/src/json/hint.rs
@@ -1,4 +1,6 @@
 use std::convert::TryFrom;
+use std::num::ParseIntError;
+use std::str::FromStr;
 
 use serde::Deserialize;
 use serde::Serialize;
@@ -12,15 +14,27 @@ pub struct CommitmentHintJson {}
 pub struct NodePositionJson(String);
 
 impl From<NodePosition> for NodePositionJson {
-    fn from(_: NodePosition) -> Self {
-        todo!()
+    fn from(p: NodePosition) -> Self {
+        NodePositionJson(
+            p.positions
+                .into_iter()
+                .map(|p| p.to_string())
+                .collect::<Vec<String>>()
+                .join("-"),
+        )
     }
 }
 
 impl TryFrom<NodePositionJson> for NodePosition {
-    type Error = String;
+    type Error = ParseIntError;
 
-    fn try_from(value: NodePositionJson) -> Result<Self, Self::Error> {
-        todo!()
+    fn try_from(pj: NodePositionJson) -> Result<Self, Self::Error> {
+        Ok(NodePosition {
+            positions: pj
+                .0
+                .split('-')
+                .map(usize::from_str)
+                .collect::<Result<Vec<usize>, _>>()?,
+        })
     }
 }
