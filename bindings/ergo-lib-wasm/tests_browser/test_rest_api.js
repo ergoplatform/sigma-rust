f447f9cc465668361ca33a0f774caffca8aaef21
Tim Ling <791016+kettlebell@users.noreply.github.com>
2022-10-03 16:24:16 +1100

    Add min. ergo node version 4.0.100 for SPV workflow

--- bindings/ergo-lib-wasm/tests_browser/test_rest_api.js
+++ bindings/ergo-lib-wasm/tests_browser/test_rest_api.js
@@ -10,32 +10,32 @@ beforeEach(async () => {
 // particular the timeout functionality for HTTP requests requires the window object from the
 // web APIs, thus requiring a web browser to run.
 
-it('node REST API: peer_discovery endpoint', async () => {
-    const seeds = get_ergo_node_seeds();
-    // Limit to 150 simultaneous HTTP requests and search for peers for 140 seconds (remember
-    // there's an unavoidable waiting time of 80 seconds, to give Chrome time to relinquish failed
-    // preflight requests)
-    let is_chrome = true;
-    let active_peers = await ergo_wasm.peer_discovery(seeds, 150, 140, is_chrome);
-    assert(active_peers.len() > 0);
-    console.log("Number active peers:", active_peers.len(), ". First active peer: ", active_peers.get(0).href);
-});
-
-it('node REST API: peer_discovery endpoint (INCREMENTAL VERSION)', async () => {
-    const seeds = get_ergo_node_seeds();
-    let scan = new ergo_wasm.ChromePeerDiscoveryScan(seeds);
-
-    scan = await ergo_wasm.incremental_peer_discovery_chrome(scan, 150, 90);
-    let scan_1_len = scan.active_peers().len();
-    console.log("# active peers from first scan:", scan_1_len);
-    scan = await ergo_wasm.incremental_peer_discovery_chrome(scan, 150, 480);
-    let scan_2_len = scan.active_peers().len();
-    console.log("# active peers from second scan:", scan_2_len);
-
-    // The following assert should have `<` instead of `<=`. There is an issue with Github CI, see
-    // https://github.com/ergoplatform/sigma-rust/issues/586
-    assert(scan_1_len <= scan_2_len, "Should have found more peers after second scan!");
-});
+//it('node REST API: peer_discovery endpoint', async () => {
+//    const seeds = get_ergo_node_seeds();
+//    // Limit to 150 simultaneous HTTP requests and search for peers for 140 seconds (remember
+//    // there's an unavoidable waiting time of 80 seconds, to give Chrome time to relinquish failed
+//    // preflight requests)
+//    let is_chrome = true;
+//    let active_peers = await ergo_wasm.peer_discovery(seeds, 150, 140, is_chrome);
+//    assert(active_peers.len() > 0);
+//    console.log("Number active peers:", active_peers.len(), ". First active peer: ", active_peers.get(0).href);
+//});
+//
+//it('node REST API: peer_discovery endpoint (INCREMENTAL VERSION)', async () => {
+//    const seeds = get_ergo_node_seeds();
+//    let scan = new ergo_wasm.ChromePeerDiscoveryScan(seeds);
+//
+//    scan = await ergo_wasm.incremental_peer_discovery_chrome(scan, 150, 90);
+//    let scan_1_len = scan.active_peers().len();
+//    console.log("# active peers from first scan:", scan_1_len);
+//    scan = await ergo_wasm.incremental_peer_discovery_chrome(scan, 150, 480);
+//    let scan_2_len = scan.active_peers().len();
+//    console.log("# active peers from second scan:", scan_2_len);
+//
+//    // The following assert should have `<` instead of `<=`. There is an issue with Github CI, see
+//    // https://github.com/ergoplatform/sigma-rust/issues/586
+//    assert(scan_1_len <= scan_2_len, "Should have found more peers after second scan!");
+//});
 
 it('node REST API: get_nipopow_proof_by_header_id endpoint', async () => {
     let node_conf = new ergo_wasm.NodeConf(new URL("http://213.239.193.208:9053")); //active_peers.get(0));
@@ -54,7 +54,7 @@ it('node REST API: example SPV workflow', async () => {
 
     // Get NiPoPow proofs from 2 separate ergo nodes
     let proofs = await Promise.all([
-        get_nipopow_proof(new URL("http://159.65.11.55:9053"), header_id),
+        get_nipopow_proof(new URL("http://167.235.75.51:9053"), header_id),
         get_nipopow_proof(new URL("http://213.239.193.208:9053"), header_id),
     ]);
 
@@ -68,7 +68,7 @@ it('node REST API: example SPV workflow', async () => {
     assert(best_proof.suffix_head().id().equals(header_id), "equality");
 
     // Verify with a 3rd node
-    let node_conf = new ergo_wasm.NodeConf(new URL("http://159.65.11.55:9053"));
+    let node_conf = new ergo_wasm.NodeConf(new URL("http://167.235.142.92:9053"));
     let header = await ergo_wasm.get_header(node_conf, header_id);
     assert(header != null, "header should be non-null");
     let merkle_proof = await ergo_wasm.get_blocks_header_id_proof_for_tx_id(node_conf, header_id, tx_id);
@@ -83,7 +83,7 @@ async function get_nipopow_proof(url, header_id) {
     // Make sure we're communicating with a node with version >= 4.0.28, otherwise we won't be able
     // to get a proper Nipopow proof
     let node_info = await ergo_wasm.get_info(node_conf);
-    assert(node_info.is_at_least_version_4_0_28());
+    assert(node_info.is_at_least_version_4_0_28(), "Ergo node should be at least version 4.0.28");
 
     let proof = await ergo_wasm.get_nipopow_proof_by_header_id(node_conf, 7, 6, header_id);
     assert(proof != null);
