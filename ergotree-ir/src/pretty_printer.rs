8b3d624dca96526f094ccb6cc108a88f5db60031
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2023-07-24 16:13:16 +0300

    draft Print trait IR nodes;

--- ergotree-ir/src/pretty_printer.rs
+++ ergotree-ir/src/pretty_printer.rs
@@ -1,16 +1,54 @@
+use std::fmt::Write;
+
+use crate::mir::coll_append::Append;
 use crate::mir::expr::Expr;
+use crate::source_span::Span;
+use crate::source_span::Spanned;
 
 pub(crate) struct PrintExpr {
-    expr: Expr,
+    spanned_expr: Expr,
     text: String,
 }
 
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub(crate) enum PrintError {}
 
-#[allow(clippy::todo)]
-pub(crate) fn print_expr(expr: Expr) -> Result<PrintExpr, PrintError> {
-    todo!()
+struct PrintContext {
+    current_pos: usize,
+    current_indent: usize,
+}
+
+trait Print {
+    fn print(&self, w: &mut dyn Write, pc: &mut PrintContext) -> Result<Expr, PrintError>;
+}
+
+impl Print for Append {
+    fn print(&self, w: &mut dyn Write, pc: &mut PrintContext) -> Result<Expr, PrintError> {
+        let start_pos = pc.current_pos;
+        self.input.print(w, pc)?;
+        // TODO: set indent
+        write!(w, ".append(")?;
+        // TODO: need a custom writer to bundle output and moving current pos
+        pc.current_pos += 8;
+        self.col_2.print(w, pc)?;
+        write!(w, ")")?;
+        pc.current_pos += 1;
+        let end_pos = pc.current_pos;
+        Ok(Spanned {
+            source_span: Span {
+                start: start_pos,
+                end: end_pos,
+            },
+            expr: self.clone(),
+        }
+        .into())
+    }
+}
+
+impl Print for Expr {
+    fn print(&self, w: &mut dyn Write, pc: &mut PrintContext) -> Result<Expr, PrintError> {
+        todo!()
+    }
 }
 
 #[allow(clippy::unwrap_used)]
@@ -27,7 +65,13 @@ mod tests {
     use super::*;
 
     fn check(expr: Expr, expected_tree: expect_test::Expect) {
-        let expected_out = print_expr(expr).unwrap();
+        // TODO: create a formatter and grab it's output
+        let expected_out = todo!();
+        let mut pc = PrintContext {
+            current_pos: 0,
+            current_indent: 0,
+        };
+        let spanned_expr = expr.print(w, &mut pc).unwrap();
         expected_tree.assert_eq(&expected_out.text);
     }
 
