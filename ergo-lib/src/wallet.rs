e753f1bba77e8d09be009194ef444215d4f7d6d9
Rupesh Prajapati <rupesh_p@ch.iitr.ac.in>
2025-04-07 19:51:45 +0530

    fixes

--- ergo-lib/src/wallet.rs
+++ ergo-lib/src/wallet.rs
@@ -16,7 +16,7 @@ pub mod signing;
 pub mod tx_builder;
 pub mod tx_context;
 
-use crate::ergotree_interpreter::sigma_protocol::prover::hint::{ Hint, HintsBag };
+use crate::ergotree_interpreter::sigma_protocol::prover::hint::{Hint, HintsBag};
 use alloc::boxed::Box;
 use alloc::vec::Vec;
 use ergotree_interpreter::sigma_protocol::private_input::PrivateInput;
@@ -38,7 +38,7 @@ use crate::chain::transaction::Transaction;
 use crate::ergotree_ir::sigma_protocol::sigma_boolean::SigmaBoolean;
 use crate::wallet::mnemonic::Mnemonic;
 #[cfg(feature = "std")]
-use crate::wallet::multi_sig::{ generate_commitments, generate_commitments_for };
+use crate::wallet::multi_sig::{generate_commitments, generate_commitments_for};
 
 use self::ext_secret_key::ExtSecretKey;
 use self::ext_secret_key::ExtSecretKeyError;
@@ -46,7 +46,7 @@ use self::signing::sign_reduced_transaction;
 use self::signing::TransactionContext;
 use self::signing::TxSigningError;
 #[cfg(feature = "std")]
-use self::signing::{ make_context, sign_message, sign_transaction, sign_tx_input };
+use self::signing::{make_context, sign_message, sign_transaction, sign_tx_input};
 
 /// Wallet
 pub struct Wallet {
@@ -57,11 +57,14 @@ pub struct Wallet {
 #[allow(missing_docs)]
 #[derive(Error, Debug)]
 pub enum WalletError {
-    #[error("Transaction signing error: {0}")] TxSigningError(#[from] TxSigningError),
+    #[error("Transaction signing error: {0}")]
+    TxSigningError(#[from] TxSigningError),
 
-    #[error("Prover error: {0}")] ProverError(#[from] ProverError),
+    #[error("Prover error: {0}")]
+    ProverError(#[from] ProverError),
 
-    #[error("ExtSecretKeyError: {0}")] ExtSecretKeyError(#[from] ExtSecretKeyError),
+    #[error("ExtSecretKeyError: {0}")]
+    ExtSecretKeyError(#[from] ExtSecretKeyError),
 
     #[error("error parsing SecretKey from ExtSecretKey.bytes")]
     SecretKeyParsingError,
@@ -72,7 +75,7 @@ impl Wallet {
     /// Returns None if a DlogSecretKey cannot be parsed from the provided phrase
     pub fn from_mnemonic(
         mnemonic_phrase: &str,
-        mnemonic_pass: &str
+        mnemonic_pass: &str,
     ) -> Result<Wallet, WalletError> {
         let seed = Mnemonic::to_seed(mnemonic_phrase, mnemonic_pass);
         let ext_sk = ExtSecretKey::derive_master(seed)?;
@@ -100,11 +103,10 @@ impl Wallet {
         &self,
         tx_context: TransactionContext<UnsignedTransaction>,
         state_context: &ErgoStateContext,
-        tx_hints: Option<&TransactionHintsBag>
+        tx_hints: Option<&TransactionHintsBag>,
     ) -> Result<Transaction, WalletError> {
-        sign_transaction(self.prover.as_ref(), tx_context, state_context, tx_hints).map_err(
-            WalletError::from
-        )
+        sign_transaction(self.prover.as_ref(), tx_context, state_context, tx_hints)
+            .map_err(WalletError::from)
     }
 
     /// Signs a reduced transaction (generating proofs for inputs)
@@ -112,11 +114,10 @@ impl Wallet {
     pub fn sign_reduced_transaction(
         &self,
         reduced_tx: ReducedTransaction,
-        tx_hints: Option<&TransactionHintsBag>
+        tx_hints: Option<&TransactionHintsBag>,
     ) -> Result<Transaction, WalletError> {
-        sign_reduced_transaction(self.prover.as_ref(), reduced_tx, tx_hints).map_err(
-            WalletError::from
-        )
+        sign_reduced_transaction(self.prover.as_ref(), reduced_tx, tx_hints)
+            .map_err(WalletError::from)
     }
 
     /// Generate commitments for Transaction by wallet secrets
@@ -124,9 +125,10 @@ impl Wallet {
     pub fn generate_commitments(
         &self,
         tx_context: TransactionContext<UnsignedTransaction>,
-        state_context: &ErgoStateContext
+        state_context: &ErgoStateContext,
     ) -> Result<TransactionHintsBag, TxSigningError> {
-        let public_keys: Vec<SigmaBoolean> = self.prover
+        let public_keys: Vec<SigmaBoolean> = self
+            .prover
             .secrets()
             .iter()
             .map(|secret| secret.public_image())
@@ -138,10 +140,11 @@ impl Wallet {
     #[cfg(feature = "std")]
     pub fn generate_commitments_for_reduced_transaction(
         &self,
-        reduced_tx: ReducedTransaction
+        reduced_tx: ReducedTransaction,
     ) -> Result<TransactionHintsBag, TxSigningError> {
         let mut tx_hints = TransactionHintsBag::empty();
-        let public_keys: Vec<SigmaBoolean> = self.prover
+        let public_keys: Vec<SigmaBoolean> = self
+            .prover
             .secrets()
             .iter()
             .map(|secret| secret.public_image())
@@ -159,20 +162,18 @@ impl Wallet {
     pub fn generate_deterministic_commitments(
         &self,
         reduced_tx: &ReducedTransaction,
-        aux_rand: &[u8]
+        aux_rand: &[u8],
     ) -> Result<TransactionHintsBag, TxSigningError> {
         let mut tx_hints = TransactionHintsBag::empty();
         let msg = reduced_tx.unsigned_tx.bytes_to_sign()?;
         for (index, input) in reduced_tx.reduced_inputs().iter().enumerate() {
-            if
-                let Some(bag) = self::deterministic::generate_commitments_for(
-                    &*self.prover,
-                    &input.sigma_prop,
-                    &msg,
-                    aux_rand
-                )
-            {
-                tx_hints.add_hints_for_input(index, bag);
+            if let Some(bag) = self::deterministic::generate_commitments_for(
+                &*self.prover,
+                &input.sigma_prop,
+                &msg,
+                aux_rand,
+            ) {
+                tx_hints.add_hints_for_input(index, bag)
             };
         }
         Ok(tx_hints)
@@ -189,7 +190,7 @@ impl Wallet {
         &self,
         tx_context: TransactionContext<UnsignedTransaction>,
         state_context: &ErgoStateContext,
-        aux_rand: &[u8]
+        aux_rand: &[u8],
     ) -> Result<Transaction, WalletError> {
         let reduced_tx = reduce_tx(tx_context, state_context)?;
         let hints = self.generate_deterministic_commitments(&reduced_tx, aux_rand)?;
@@ -201,7 +202,7 @@ impl Wallet {
     pub fn sign_reduced_transaction_deterministic(
         &self,
         reduced_tx: ReducedTransaction,
-        aux_rand: &[u8]
+        aux_rand: &[u8],
     ) -> Result<Transaction, WalletError> {
         let hints = self.generate_deterministic_commitments(&reduced_tx, aux_rand)?;
         sign_reduced_transaction(&*self.prover, reduced_tx, Some(&hints)).map_err(From::from)
@@ -212,7 +213,7 @@ impl Wallet {
     pub fn sign_message(
         &self,
         sigma_tree: SigmaBoolean,
-        msg: &[u8]
+        msg: &[u8],
     ) -> Result<Vec<u8>, WalletError> {
         sign_message(self.prover.as_ref(), sigma_tree, msg).map_err(WalletError::from)
     }
@@ -224,24 +225,21 @@ impl Wallet {
         input_idx: usize,
         tx_context: TransactionContext<UnsignedTransaction>,
         state_context: &ErgoStateContext,
-        tx_hints: Option<&TransactionHintsBag>
+        tx_hints: Option<&TransactionHintsBag>,
     ) -> Result<Input, WalletError> {
         let tx = tx_context.spending_tx.clone();
         let message_to_sign = tx.bytes_to_sign().map_err(TxSigningError::from)?;
-        let mut context = make_context(state_context, &tx_context, input_idx).map_err(
-            TxSigningError::from
-        )?;
-        Ok(
-            sign_tx_input(
-                self.prover.as_ref(),
-                &tx_context,
-                state_context,
-                &mut context,
-                tx_hints,
-                input_idx,
-                message_to_sign.as_slice()
-            )?
-        )
+        let mut context =
+            make_context(state_context, &tx_context, input_idx).map_err(TxSigningError::from)?;
+        Ok(sign_tx_input(
+            self.prover.as_ref(),
+            &tx_context,
+            state_context,
+            &mut context,
+            tx_hints,
+            input_idx,
+            message_to_sign.as_slice(),
+        )?)
     }
 }
 
